164137fab8fd4a03e7b81bd8e8c00f0693a16c27
nevinheintze <32204684+nevinheintze@users.noreply.github.com>
2020-01-23 14:17:18 +1100

    Add Batchwriter. (#1)
    Initial commit to harbourbridge repository with license/contrib files,
    an initial README (mostly empty), and the first chunk of code: batchwriter.
    
    Batchwriter is an abstraction on top of the standard Spanner Client
    library that batches mutations together and manages the writing of
    those mutations to Spanner. HarbourBridge uses this to efficiently
    write data to Spanner.

---
+++ spanner/batchwriter.go
@@ -0,0 +1,333 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package spanner provides high-level abstractions for working with
+// Cloud Spanner that are not available from the core Cloud Spanner
+// libraries.
+package spanner
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+	"unsafe"
+
+	sp "cloud.google.com/go/spanner"
+)
+
+// Parameters used to control building batches to write to Spanner.
+// Batches are built by adding rows until we hit one of the thresholds.
+// Bigger batches are usually more efficient, but we need to be careful
+// not to exceed Spanner's limits. Also, sending huge RPCs is potentially
+// unreliable.
+const (
+	countThreshold = 10 * 1000    // Spanner per-operation limit is 20,000.
+	byteThreshold  = 20 * 1 << 20 // Spanner per-operation limit is 100MB.
+)
+
+// BatchWriter accumulates rows of data (via AddRow) and assembles them
+// into batches that it asynchronously writes to Spanner.  Rows are
+// written to Spanner using insert semantics i.e. if a row already exists
+// in the database, the row will fail with error 'AlreadyExists'.  If
+// Spanner returns an error for a batch, BatchWriter splits the batch
+// into smaller chunks to retry, as it attempts to isolate which row(s)
+// in a batch is bad.  BatchWriter respects Spanner's limits on byte size
+// and mutation count and has configurable limits on the number of
+// in-progress writes, amount of data buffered and retry behavior.
+// BatchWriter is not threadsafe: only one call to AddRow or Flush should
+// be active at any time.  See ExampleBatchWriter (batchwriter_test.go)
+// for sample usage code.
+type BatchWriter struct {
+	rows       []*row                     // Buffered rows.
+	rBytes     int64                      // Estimate of bytes for buffered rows.
+	rCount     int64                      // Mutation count for buffered rows.
+	write      func([]*sp.Mutation) error // Typically a closure that calls client.Apply, but structured this way for testing.
+	wg         sync.WaitGroup             // Tracks in-progress writes.
+	writeLimit int64                      // Limit on number of in-progress writes.
+	bytesLimit int64                      // Limit on bytes buffered. AddRow blocks if rBytes exceeded this value.
+	retryLimit int64                      // Limit on retries.
+	verbose    bool                       // If true, print out messages about each write batch.
+	async      asyncState
+}
+
+type row struct {
+	table string
+	cols  []string
+	vals  []interface{}
+}
+
+// Fields in this struct are modified asynchronously e.g. by go routines writing
+// data to Spanner. Either hold a lock or use atomics, as detailed below.
+//
+// Note on terminology. A bad row is a row that generated an error. A dropped
+// row is a row that we didn't write to Spanner (either because it generated
+// an error, or because it was part of a batch that generate errors and we'd
+// exhausted our retry budget and didn't split the batch and try again).
+type asyncState struct {
+	writes             int64            // Number of in-progress writes; access using atomic.
+	retries            int64            // Number of retries; access using atomic.
+	lock               sync.Mutex       // Protects errors and badRows
+	errors             map[string]int64 // Errors encountered; protected by lock.
+	sampleBadRows      []*row           // A sample of rows that generated errors; protected by lock.
+	sampleBadRowsBytes int64            // Estimate of bytes for sampleBadRows; protected by lock.
+	droppedRows        map[string]int64 // Count of dropped rows, broken down by table.
+}
+
+// BatchWriterConfig specifies parameters for configuring BatchWriter.
+type BatchWriterConfig struct {
+	WriteLimit int64                      // Limit on number of in-progress writes.
+	BytesLimit int64                      // Limit on bytes buffered.
+	RetryLimit int64                      // Limit on retries.
+	Write      func([]*sp.Mutation) error // Function to call to write to Spanner (typically a closure that calls client.Apply).
+	Verbose    bool                       // If true, print out messages about each write batch.
+}
+
+// NewBatchWriter returns a new BatchWriter with parameters defined by config.
+func NewBatchWriter(config BatchWriterConfig) *BatchWriter {
+	return &BatchWriter{
+		write:      config.Write,
+		writeLimit: config.WriteLimit,
+		bytesLimit: config.BytesLimit,
+		retryLimit: config.RetryLimit,
+		verbose:    config.Verbose,
+		async: asyncState{
+			errors:      make(map[string]int64),
+			droppedRows: make(map[string]int64),
+		},
+	}
+}
+
+// AddRow appends a new row of data to bw's buffer of rows. Depending on the
+// state of BatchWriter, AddRow may immediately return, or it may initiate writes,
+// or it may block (waiting for some of the writes already in progress to
+// complete) and then initiate writes.
+func (bw *BatchWriter) AddRow(table string, cols []string, vals []interface{}) {
+	r := &row{table, cols, vals}
+	bw.rows = append(bw.rows, r)
+	bw.rBytes += byteSize(r)
+	bw.rCount += int64(len(r.cols))
+	bw.writeData()
+}
+
+// Flush initiates writes to Spanner of all buffered rows of data, and waits
+// for them to complete.
+func (bw *BatchWriter) Flush() {
+	for len(bw.rows) > 0 {
+		if atomic.LoadInt64(&bw.async.writes) < bw.writeLimit {
+			m, count, bytes := bw.getBatch()
+			if bw.verbose {
+				fmt.Printf("Starting write of %d rows to Spanner (%d bytes, %d mutations) [%d in progress]\n",
+					len(m), bytes, count, atomic.LoadInt64(&bw.async.writes))
+			}
+			bw.startWrite(m)
+		} else {
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+	bw.wg.Wait()
+}
+
+// DroppedRowsByTable returns a map of tables to counts of dropped rows.
+// Dropped rows are rows that were not written to Spanner.
+func (bw *BatchWriter) DroppedRowsByTable() map[string]int64 {
+	// Make a copy of bw.async.droppedRows since it is not thread-safe.
+	m := make(map[string]int64)
+	bw.async.lock.Lock()
+	defer bw.async.lock.Unlock()
+
+	for t, n := range bw.async.droppedRows {
+		m[t] = n
+	}
+	return m
+}
+
+// SampleBadRows returns a string-formatted list of sample rows that
+// generated errors. Returns at most n rows.
+// Note that we split up batches to isolate errors. Each row returned
+// by SampleBadRows generated an error when sent to Spanner as a
+// single-row batch.
+func (bw *BatchWriter) SampleBadRows(n int) []string {
+	var l []string
+	bw.async.lock.Lock()
+	defer bw.async.lock.Unlock()
+	for _, x := range bw.async.sampleBadRows {
+		if len(l) >= n {
+			break
+		}
+		l = append(l, fmt.Sprintf("table=%s cols=%v data=%v", x.table, x.cols, x.vals))
+	}
+	return l
+}
+
+// Errors returns a map summarizing errors encountered. Keys are error
+// strings, and values are the count of that error.
+func (bw *BatchWriter) Errors() map[string]int64 {
+	// Make a copy of bw.async.errors since it is not thread-safe.
+	m := make(map[string]int64)
+	bw.async.lock.Lock()
+	defer bw.async.lock.Unlock()
+	for k, v := range bw.async.errors {
+		m[k] = v
+	}
+	return m
+}
+
+func (bw *BatchWriter) getBadRowsForTest() []*row {
+	return bw.async.sampleBadRows
+}
+
+// getBatch returns a slice of data from the front of bw.rows.  The slice
+// returned is the largest one not exceeding countThreshold and byteThreshold.
+func (bw *BatchWriter) getBatch() (rows []*row, count int64, bytes int64) {
+	for i, _ := range bw.rows {
+		c := count + int64(len(bw.rows[i].cols))
+		b := bytes + byteSize(bw.rows[i])
+		// If next row puts us over the thresholds, then stop. But make sure
+		// we have at least one row. If a single row puts us over the
+		// thresholds, there's not much we can do: we just try sending it to Spanner
+		// (it might succeed, since our thresholds are conservative).
+		if (c >= countThreshold || b >= byteThreshold) && len(rows) >= 1 {
+			bw.rCount -= count
+			bw.rBytes -= bytes
+			bw.rows = bw.rows[i:]
+			return rows, count, bytes
+		}
+		count = c
+		bytes = b
+		rows = append(rows, bw.rows[i])
+	}
+	bw.rCount = 0
+	bw.rBytes = 0
+	bw.rows = nil
+	return rows, count, bytes
+}
+
+func (bw *BatchWriter) errorStats(rows []*row, err error, retry bool) {
+	if bw.verbose {
+		fmt.Printf("Error while writing %d rows to Spanner: %v\n", len(rows), err)
+	}
+
+	bw.async.lock.Lock()
+	defer bw.async.lock.Unlock()
+
+	bw.async.errors[err.Error()]++
+	if retry {
+		return
+	}
+	// All rows in r will be dropped.
+	if len(rows) == 1 {
+		// This is a confirmed bad row: add it to the badRows list.
+		r := rows[0]
+		n := byteSize(r)
+		// Use bw.bytesLimit to cap storage used by badRows. Keep at least one bad row.
+		if bw.async.sampleBadRowsBytes+n < bw.bytesLimit || len(bw.async.sampleBadRows) == 0 {
+			bw.async.sampleBadRows = append(bw.async.sampleBadRows, r)
+			bw.async.sampleBadRowsBytes += n
+		}
+	}
+	for _, x := range rows {
+		bw.async.droppedRows[x.table]++
+	}
+	return
+}
+
+// Note: doWriteAndHandleErrors must be thread-safe because it is run
+// inside a go routine.
+func (bw *BatchWriter) doWriteAndHandleErrors(rows []*row) {
+	var m []*sp.Mutation
+	for _, x := range rows {
+		m = append(m, sp.Insert(x.table, x.cols, x.vals))
+	}
+	if err := bw.write(m); err != nil {
+		hitRetryLimit := atomic.LoadInt64(&bw.async.retries) >= bw.retryLimit
+		retry := len(rows) > 1 && !hitRetryLimit
+		bw.errorStats(rows, err, retry)
+		if !retry {
+			if hitRetryLimit && bw.verbose {
+				fmt.Printf("Have hit %d retries: will not do any more\n", atomic.LoadInt64(&bw.async.retries))
+			}
+			return
+		}
+		// Split into 10 pieces and retry. This is useful
+		// if a batch contains a bad data row (Spanner
+		// will fail the entire batch). In effect we attempt
+		// to narrow down which row (or rows) are bad, and
+		// write the 'good' rows to Spanner.
+		k := 1 + len(rows)/10
+		min := func(i, j int) int {
+			if i <= j {
+				return i
+			}
+			return j
+		}
+		for i := 0; i < len(rows); i += k {
+			atomic.AddInt64(&bw.async.retries, 1)
+			bw.doWriteAndHandleErrors(rows[i:min(i+k, len(rows))])
+		}
+	}
+}
+
+// Note: backgroundWrite must be thread-safe because it is run as
+// a go routine.
+func (bw *BatchWriter) backgroundWrite(rows []*row) {
+	defer bw.wg.Done()
+	defer atomic.AddInt64(&bw.async.writes, -1)
+	bw.doWriteAndHandleErrors(rows)
+}
+
+// startWrite initiates an asynchronous write of rows to Spanner.
+func (bw *BatchWriter) startWrite(rows []*row) {
+	bw.wg.Add(1)
+	atomic.AddInt64(&bw.async.writes, 1)
+	go bw.backgroundWrite(rows)
+}
+
+// writeData initiates writes to Spanner until either:
+// a) we have less than a 'batch' to write, or
+// b) we've hit writeLimit and we're under bytesLimit.
+// It will block and re-try till either (a) or (b) holds.
+func (bw *BatchWriter) writeData() {
+	for bw.rCount > countThreshold || bw.rBytes > byteThreshold {
+		if atomic.LoadInt64(&bw.async.writes) < bw.writeLimit {
+			m, count, bytes := bw.getBatch()
+			if bw.verbose {
+				fmt.Printf("Starting write of %d rows to Spanner (%d bytes, %d mutations) [%d in progress]\n",
+					len(m), bytes, count, atomic.LoadInt64(&bw.async.writes))
+			}
+			bw.startWrite(m)
+		} else {
+			if bw.rBytes < bw.bytesLimit {
+				return
+			}
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+}
+
+func byteSize(r *row) int64 {
+	n := int64(len(r.table))
+	for _, c := range r.cols {
+		n += int64(len(c))
+	}
+	for _, v := range r.vals {
+		switch x := v.(type) {
+		case string:
+			n += int64(len(x))
+		default:
+			n += int64(unsafe.Sizeof(v))
+		}
+	}
+	return n
+}
---
+++ spanner/batchwriter_test.go
@@ -0,0 +1,234 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package spanner
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"sort"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	sp "cloud.google.com/go/spanner"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestFlush tests NewBatchWriter, AddRow and Flush.
+func TestFlush(t *testing.T) {
+	tests := []struct {
+		name        string
+		count       int
+		rowSize     int
+		writeLimit  int64
+		badRowIndex map[int]bool // Identifies which rows are bad (by index).
+	}{
+		{name: "One write", count: 1, rowSize: 5, writeLimit: 40},
+		{name: "Many writes", count: 50000, rowSize: 5, writeLimit: 40},      // Forces split based on mutation count.
+		{name: "Large writes", count: 100, rowSize: 1 << 20, writeLimit: 40}, // Forces split based on byte size.
+		{name: "Write limit", count: 50000, rowSize: 5, writeLimit: 5},       // Forces write-limiting.
+		{name: "Bad rows", count: 50, rowSize: 5, writeLimit: 40, badRowIndex: map[int]bool{6: true, 17: true}},
+	}
+	config := BatchWriterConfig{
+		BytesLimit: 100 << 20,
+		Verbose:    false,
+		RetryLimit: 1000,
+	}
+	for _, tc := range tests {
+		data, limit := generateRows(tc.count, tc.rowSize)
+		goodRows, badRows := partitionRows(tc.badRowIndex, data)
+		badMutations := toMutations(badRows)
+		mutex := &sync.Mutex{}
+		var writeCount int64
+		var rowsWritten []*sp.Mutation
+		config.WriteLimit = tc.writeLimit
+		config.Write = func(m []*sp.Mutation) error {
+			var err error
+			assert.LessOrEqual(t, len(m), limit, fmt.Sprintf("%s: Too many rows in write", tc.name))
+			mutex.Lock()
+			writeCount++
+			// intersect could be slow if badMutations is more than a few rows.
+			if intersect(m, badMutations) {
+				err = errors.New("bad data")
+			} else {
+				rowsWritten = append(rowsWritten, m...)
+			}
+			mutex.Unlock()
+			time.Sleep(20 * time.Millisecond) // Mimic a Spanner write.
+			mutex.Lock()
+			assert.LessOrEqual(t, writeCount, tc.writeLimit, fmt.Sprintf("%s: Too many pending writes", tc.name))
+			writeCount--
+			mutex.Unlock()
+			return err
+		}
+		bw := NewBatchWriter(config)
+		for _, x := range data {
+			bw.AddRow(x.table, x.cols, x.vals)
+		}
+		bw.Flush()
+
+		// Check data written.
+		expected := toMutations(goodRows)
+		actual := rowsWritten
+		equalMutations(t, expected, actual, tc.name+" (good data)")
+
+		// Check data rejected.
+		expected = toMutations(badRows)
+		actual = toMutations(bw.getBadRowsForTest()) // All go routines are done, so we can access bw internals.
+		equalMutations(t, expected, actual, tc.name+" (bad data)")
+	}
+}
+
+func TestDroppedRowsByTable(t *testing.T) {
+	bw := NewBatchWriter(BatchWriterConfig{})
+	bw.async.lock.Lock()
+	bw.async.droppedRows["test1"] = 22
+	bw.async.droppedRows["test2"] = 42
+	bw.async.lock.Unlock()
+	m := bw.DroppedRowsByTable()
+	assert.Equal(t, 2, len(m))
+	assert.Equal(t, int64(22), m["test1"])
+	assert.Equal(t, int64(42), m["test2"])
+}
+
+func TestSampleBadRows(t *testing.T) {
+	bw := NewBatchWriter(BatchWriterConfig{})
+	bw.async.lock.Lock()
+	bw.async.sampleBadRows = []*row{
+		&row{"test", []string{"col1", "col2"}, []interface{}{"a", int64(42)}},
+		&row{"test", []string{"col1", "col2"}, []interface{}{"b", int64(6)}},
+	}
+	bw.async.lock.Unlock()
+	l := bw.SampleBadRows(1)
+	assert.Equal(t, l, []string{"table=test cols=[col1 col2] data=[a 42]"})
+}
+
+func TestErrors(t *testing.T) {
+	bw := NewBatchWriter(BatchWriterConfig{})
+	bw.async.lock.Lock()
+	bw.async.errors["error string 1"] = 22
+	bw.async.errors["error string 2"] = 42
+	bw.async.lock.Unlock()
+	m := bw.Errors()
+	assert.Equal(t, 2, len(m))
+	assert.Equal(t, int64(22), m["error string 1"])
+	assert.Equal(t, int64(42), m["error string 2"])
+}
+
+func ExampleBatchWriter() {
+	write := func(m []*sp.Mutation) error {
+		var err error
+		// Code to write to Spanner e.g.
+		// client := sp.NewClient(...)
+		// _, err = client.Apply(context.Background(), m)
+		return err
+	}
+	config := BatchWriterConfig{
+		WriteLimit: 40,            // Limit on number of in-progress writes; 40 is a good default.
+		BytesLimit: 100 * 1 << 20, // Limit on bytes buffered; 100MB is a good default.
+		RetryLimit: 1000,          // Limit on retries (if a large set of mutations fails, we split it into smaller pieces and re-try).
+		Write:      write,
+		Verbose:    false, // Whether to print messages about each write.
+	}
+	writer := NewBatchWriter(config)
+
+	// Code to generate rows of data to write to Spanner.
+	cols := []string{"id", "quote"}
+	vals := []interface{}{42, "The answer to life the universe and everything."}
+	writer.AddRow("mytable", cols, vals)
+	vals = []interface{}{6, "I am not a number."}
+	writer.AddRow("mytable", cols, vals)
+	// End of code to generate rows.
+
+	writer.Flush() // Flush out remaining writes and wait for them to complete.
+}
+
+func generateRows(count int, size int) ([]*row, int) {
+	var r []*row
+	cols := []string{"a", "b"}
+	val := strings.Repeat("x", size)
+	for i := 0; i < count; i++ {
+		// vals[0] serves as a unique id for each row.
+		vals := []interface{}{i, val}
+		r = append(r, &row{"table", cols, vals})
+	}
+	// Find the max number of rows in a write for the (fixed sized)
+	// rows generated in this test data.
+	limitCount := countThreshold / len(cols) // Compute limit based on mutation count.
+	limitBytes := byteThreshold / (8 + size) // Compute limit based on mutation bytes.
+	if limitCount <= limitBytes {
+		return r, limitCount
+	}
+	return r, limitBytes
+}
+
+// equalMutations is a fast way to check that two slices of mutations are
+// the same (reflect.deepEquals) except for re-ordering.
+// Note that we should use assert.ElementsMatch, but it is very, very slow
+// for the sizes of slices we use here (up to 50K). We convert the mutations
+// to strings and sort them, and then use assert.Equal (ElementsMatch
+// probably can't do this).
+func equalMutations(t *testing.T, expected []*sp.Mutation, actual []*sp.Mutation, name string) {
+	toStrings := func(m []*sp.Mutation) []string {
+		var s []string
+		for _, x := range m {
+			s = append(s, fmt.Sprintf("%+v", x))
+		}
+		return s
+	}
+	e := toStrings(expected)
+	a := toStrings(actual)
+	sort.Slice(e, func(i, j int) bool { return e[i] < e[j] })
+	sort.Slice(a, func(i, j int) bool { return a[i] < a[j] })
+	assert.Equal(t, a, e, name)
+}
+
+// intersect returns true if m1 and m2 have a common element.
+func intersect(m1 []*sp.Mutation, m2 []*sp.Mutation) bool {
+	if m1 == nil || m2 == nil {
+		return false
+	}
+	for _, x := range m1 {
+		for _, y := range m2 {
+			if reflect.DeepEqual(x, y) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func toMutations(r []*row) []*sp.Mutation {
+	var m []*sp.Mutation
+	for _, x := range r {
+		m = append(m, sp.Insert(x.table, x.cols, x.vals))
+	}
+	return m
+}
+
+// partitionRows splits data into goodRows and badRows based on badRowIndex,
+// which specifies the indices of bad rows.
+func partitionRows(badRowIndex map[int]bool, data []*row) (goodRows []*row, badRows []*row) {
+	for i, _ := range data {
+		if badRowIndex[i] {
+			badRows = append(badRows, data[i])
+		} else {
+			goodRows = append(goodRows, data[i])
+		}
+	}
+	return goodRows, badRows
+}
