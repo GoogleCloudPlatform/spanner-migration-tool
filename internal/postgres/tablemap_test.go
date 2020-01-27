6e653789e96ea2d4ec659036de692f2f49b960f7
Nevin Heintze <nch@google.com>
2020-01-27 12:33:31 +1100

    Refactor table_map test.
    Make tests more explicit: specify the expected Spanner table and
    column in each test. Also, merge in the uniqueness tests.

--- internal/postgres/tablemap_test.go
+++ internal/postgres/tablemap_test.go
@@ -15,7 +15,6 @@
 package postgres
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -27,16 +26,19 @@ func TestGetSpannerTable(t *testing.T) {
 		name    string // Name of test.
 		pgTable string // PostgreSQL table name to test.
 		error   bool   // Whether an error is expected.
-		changed bool   // Should table name to be changed.
+		spTable string // Expected Spanner table name.
 	}{
-		{"Empty", "", true, false},
-		{"Good name", "table", false, false},
-		{"Illegal character", "tab\nle", false, true},
-		{"Illegal character with collision (1)", "tab\tle", false, true},
-		{"Illegal character with collision (2)", "tab?le", false, true},
-		{"Illegal start character", "2table", false, true},
-		{"Illegal start character with collision (1)", "_table", false, true},
-		{"Illegal start character with collision (2)", "\ntable", false, true},
+		{"Empty", "", true, ""},
+		{"Good name", "table", false, "table"},
+		{"Good name: setup collision", "tab_le_5", false, "tab_le_5"},
+		{"Good name: setup collision (2)", "tab_le_6", false, "tab_le_6"},
+		{"Illegal character", "tab\nle", false, "tab_le"},
+		{"Illegal character with collision (1)", "tab\tle", false, "tab_le_4"},
+		{"Illegal character with collision (2)", "tab?le", false, "tab_le_7"}, // Must skip tab_le_5 and tab_le_6.
+		{"Collision with previous remapping", "tab_le_4", false, "tab_le_4_6"},
+		{"Illegal start character", "2table", false, "Atable"},
+		{"Illegal start character with collision (1)", "_table", false, "Atable_8"},
+		{"Illegal start character with collision (2)", "\ntable", false, "Atable_9"},
 	}
 	for _, tc := range basicTests {
 		spTable, err := GetSpannerTable(conv, tc.pgTable)
@@ -44,36 +46,11 @@ func TestGetSpannerTable(t *testing.T) {
 			assert.NotNil(t, err, tc.name)
 			continue
 		}
-		assert.Equal(t, tc.changed, spTable != tc.pgTable, tc.name)
+		assert.Equal(t, tc.spTable, spTable, tc.name)
 		// Run again to check we get same result.
 		s2, err := GetSpannerTable(conv, tc.pgTable)
 		assert.Nil(t, err, tc.name)
 		assert.Equal(t, spTable, s2, tc.name)
-	}
-	// Tests that unique PostgreSQL tables are mapped to unique Spanner tables.
-	uniquenessTests := []string{"tab\nle", "tab\tle", "tab?le", "tab_le", "tab;le"}
-	set := make(map[string]bool) // Set of Spanner tables generated.
-	for _, tc := range uniquenessTests {
-		spTable, err := GetSpannerTable(conv, tc)
-		assert.Nil(t, err, fmt.Sprintf("table %s generated unexpected error", tc))
-		assert.False(t, set[spTable], fmt.Sprintf("table %s mapped to existing Spanner table %s", tc, spTable))
-		set[spTable] = true
-	}
-	// Tests the collision avoidance mechanism in GetSpannerTable.
-	// This test depends on the specific avoidance algorithm used in GetSpannerTable.
-	conv = MakeConv() // Fresh conv (clean state)
-	collisionTests := []struct {
-		in       string
-		expected string
-	}{
-		{"ta_ble", "ta_ble"},
-		{"ta_ble_2", "ta_ble_2"},
-		{"ta?ble", "ta_ble_3"}, // First we try "ta_ble", then "ta_ble_2" and finally "ta_ble_3".
-	}
-	for _, tc := range collisionTests {
-		spTable, err := GetSpannerTable(conv, tc.in)
-		assert.Nil(t, err, fmt.Sprintf("table %s generated unexpected error", tc.in))
-		assert.Equal(t, tc.expected, spTable, fmt.Sprintf("Table collision avoidance test failed: %s", tc.in))
 	}
 }
 
@@ -81,21 +58,24 @@ func TestGetSpannerCol(t *testing.T) {
 	conv := MakeConv()
 	basicTests := []struct {
 		name    string // Name of test.
-		pgTable string // PostgreSQL table name to test
-		pgCol   string // PostgreSQL col name to test
+		pgTable string // PostgreSQL table name to test.
+		pgCol   string // PostgreSQL col name to test.
 		error   bool   // Whether an error is expected.
-		changed bool   // Should col name to be changed.
+		spCol   string // Expected Spanner column name.
 	}{
-		{"Empty table", "", "col", true, false},
-		{"Empty col", "table", "", true, false},
-		{"Acceptable name", "table", "col", false, false},
-		{"Bad table", "ta.b\nle", "c\nol", false, true},
-		{"Bad col", "table", "c\nol", false, true},
-		{"Bad table and col", "t.able", "c\no\nl", false, true},
-		{"Collision 1", "table", "c\tol", false, true},
-		{"Collosion 2", "table", "c?ol", false, true},
-		{"Acceptable name 2", "table1", "col", false, false},
-		{"Collision 3", "table1", "c\nol", false, true},
+		{"Empty table", "", "col", true, ""},
+		{"Empty col", "table", "", true, ""},
+		{"Good name", "table", "col", false, "col"},
+		{"Bad table", "ta.b\nle", "col", false, "col"},
+		{"Bad col", "table", "c\nol", false, "c_ol"},
+		{"Bad table and col", "t.able", "c\no\nl", false, "c_o_l"},
+		{"table1 good name 1", "table1", "col", false, "col"},
+		{"table1 good name 2", "table1", "c_ol", false, "c_ol"},
+		{"table1 good name 3", "table1", "c_ol_5", false, "c_ol_5"},
+		{"table1 good name 4", "table1", "c_ol_6", false, "c_ol_6"},
+		{"table1 collision 1", "table1", "c\tol", false, "c_ol_4"},
+		{"table1 collision 2", "table1", "c\nol", false, "c_ol_7"}, // Skip c_ol_5 and c_ol_6.
+		{"table1 collision 3", "table1", "c?ol", false, "c_ol_8"},
 	}
 	for _, tc := range basicTests {
 		_, err1 := GetSpannerTable(conv, tc.pgTable) // Ensure table is known.
@@ -104,51 +84,10 @@ func TestGetSpannerCol(t *testing.T) {
 			assert.True(t, err1 != nil || err2 != nil, tc.name)
 			continue
 		}
-		assert.Equal(t, tc.changed, spCol != tc.pgCol, tc.name)
+		assert.Equal(t, tc.spCol, spCol, tc.name)
 		// Run again to check we get same result.
 		spCol2, err := GetSpannerCol(conv, tc.pgTable, tc.pgCol, false)
 		assert.Nil(t, err, tc.name)
 		assert.Equal(t, spCol, spCol2, tc.name)
 	}
-	// Tests that unique PostgreSQL tables/cols are mapped to unique Spanner tables/cols.
-	uniquenessTests := []struct {
-		pgTable string // PostgreSQL table name to test
-		pgCol   string // PostgreSQL col name to test
-	}{
-		{"table", "c\nol"},
-		{"table", "c\tol"},
-		{"table", "c?ol"},
-		{"table1", "col"},
-		{"table1", "c\nol"},
-		{"table1", "c?ol"},
-	}
-	set := make(map[string]bool) // Tracks set of Spanner table/col pairs.
-	for _, tc := range uniquenessTests {
-		spTable, err := GetSpannerTable(conv, tc.pgTable) // Ensure table is known.
-		assert.Nil(t, err, fmt.Sprintf("table %s generated unexpected error", tc.pgTable))
-		spCol, err := GetSpannerCol(conv, tc.pgTable, tc.pgCol, false)
-		assert.Nil(t, err, fmt.Sprintf("table %s col %s generated unexpected error", tc.pgTable, tc.pgCol))
-		assert.False(t, set[spTable+spCol], fmt.Sprintf("table %s col %s mapped to existing Spanner col %s", tc.pgTable, tc.pgCol, spCol))
-		set[spTable+spCol] = true
-	}
-	// Tests the collision avoidance mechanism in GetSpannerCol.
-	// This test depends on the specific avoidance algorithm used in GetSpannerTable.
-	conv = MakeConv() // Fresh conv (clean state)
-	table := "table"
-	_, err := GetSpannerTable(conv, table) // Ensure table is known.
-	assert.Nil(t, err, fmt.Sprintf("table %s generated unexpected error", table))
-	collisionTests := []struct {
-		in       string
-		expected string
-	}{
-		{"co_l", "co_l"},
-		{"co_l_2", "co_l_2"},
-		{"co?l", "co_l_3"}, // First we try "co_l", then "co_l_2" and finally "co_l_3".
-	}
-	for _, tc := range collisionTests {
-		spCol, err := GetSpannerCol(conv, table, tc.in, false)
-		assert.Nil(t, err, fmt.Sprintf("table %s generated unexpected error", tc.in))
-		assert.Equal(t, tc.expected, spCol, fmt.Sprintf("Column collision avoidance test failed: %s", tc.in))
-	}
-
 }
