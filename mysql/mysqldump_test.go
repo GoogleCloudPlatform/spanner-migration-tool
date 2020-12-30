cd3ce6392da1396bf1f7a21194f15d5d23a67d09
nevinheintze <32204684+nevinheintze@users.noreply.github.com>
2020-12-30 16:24:31 +1100

    Revert "Revert "#87: feat: support NUMERIC (#98)" (#102)"
    This reverts commit e24e392f6f0c2e15a136d81814122efb4c467368.

--- mysql/mysqldump_test.go
+++ mysql/mysqldump_test.go
@@ -17,6 +17,7 @@ package mysql
 import (
 	"bufio"
 	"fmt"
+	"math/big"
 	"math/bits"
 	"strings"
 	"testing"
@@ -42,7 +43,7 @@ func TestProcessMySQLDump_Scalar(t *testing.T) {
 		{"blob", ddl.Type{Name: ddl.Bytes, Len: ddl.MaxLength}},
 		{"char(42)", ddl.Type{Name: ddl.String, Len: int64(42)}},
 		{"date", ddl.Type{Name: ddl.Date}},
-		{"decimal(4,10)", ddl.Type{Name: ddl.Float64}},
+		{"decimal(4,10)", ddl.Type{Name: ddl.Numeric}},
 		{"double(4,10)", ddl.Type{Name: ddl.Float64}},
 		{"float(4,10)", ddl.Type{Name: ddl.Float64}},
 		{"integer", ddl.Type{Name: ddl.Int64}},
@@ -557,7 +558,7 @@ CREATE TABLE test (a text PRIMARY KEY, b text);`,
 	INSERT INTO test (id, a, b, c) VALUES (1,'2019-10-29',4.444,5.44444);
 	`,
 			expectedData: []spannerData{
-				spannerData{table: "test", cols: []string{"id", "a", "b", "c"}, vals: []interface{}{int64(1), getDate("2019-10-29"), float64(4.444), float64(5.44444)}}},
+				spannerData{table: "test", cols: []string{"id", "a", "b", "c"}, vals: []interface{}{int64(1), getDate("2019-10-29"), float64(4.444), mkRat(t, "5.44444")}}},
 		},
 		{
 			name: "Data conversion: smallint, mediumint, bigint, double",
@@ -780,4 +781,11 @@ func normalizeSpace(s string) string {
 
 func bitReverse(i int64) int64 {
 	return int64(bits.Reverse64(uint64(i)))
+}
+
+func mkRat(t *testing.T, v string) *big.Rat {
+	r := new(big.Rat)
+	_, ok := r.SetString(v)
+	assert.True(t, ok, fmt.Sprintf("Can't convert %q to big.Rat", v))
+	return r
 }
