c61cccf7704b9739196fea85d15fb6dc647f3d7a
shreya.arora <shreya.arora@searce.com>
2021-03-23 12:19:58 +0530

    - indentation changed for store service file

--- frontend/js/services/Store.service.js
+++ frontend/js/services/Store.service.js
@@ -3,27 +3,27 @@ import Fetch from "./Fetch.service.js";
 
 const DEFAULT_INSTANCE = {
     currentMainPageModal: null, // "name", null
-    
+
 }
 
-const Store = (function() {
+const Store = (function () {
 
     var instance;
     let sessionData = [
         {
-          sessionName : "File1.json",
-          sessionDate : "2017-01-01" ,
-          sessionTime : "1:2:3",
-          sessionAction : "resume_sesssion_url"
-      },
-      {
-        sessionName : "File1.json",
-        sessionDate : "2017-01-01" ,
-        sessionTime : "1:2:3",
-        sessionAction : "resume_sesssion_url"
-      },
-      ]
-      let modalId = "connectToDbModal"
+            sessionName: "File1.json",
+            sessionDate: "2017-01-01",
+            sessionTime: "1:2:3",
+            sessionAction: "resume_sesssion_url"
+        },
+        {
+            sessionName: "File1.json",
+            sessionDate: "2017-01-01",
+            sessionTime: "1:2:3",
+            sessionAction: "resume_sesssion_url"
+        },
+    ]
+    let modalId = "connectToDbModal"
 
     function init() {
         // the initial data from the fetch service
@@ -33,7 +33,7 @@ const Store = (function() {
     }
 
     return {
-        getinstance: function() {
+        getinstance: function () {
             if (!instance) {
                 instance = init();
             }
@@ -43,18 +43,18 @@ const Store = (function() {
         // may be later can be moved to actions and stiched to affect the store
         addAttrToStore: () => {
             if (!instance) { return; }
-            instance = {...instance, something: 'of value' }
+            instance = { ...instance, something: 'of value' }
         },
         toggleStore: () => {
             if (!instance) { return; }
             let openVal = instance.open;
             if (instance.open === 'no') {
                 openVal = 'yes';
-            } 
+            }
             else {
                 openVal = 'no';
             }
-            instance = {...instance, open: openVal };
+            instance = { ...instance, open: openVal };
         },
         addNewSession: (session) => {
             sessionData.append(session);
@@ -62,7 +62,7 @@ const Store = (function() {
         getSessionData: (index) => {
             return sessionData[index];
         },
-        getAllSessions : () => {
+        getAllSessions: () => {
             return sessionData;
         }
     };
