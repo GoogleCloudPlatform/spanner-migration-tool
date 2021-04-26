496c4f49f249c12f464e00d6ac2312061f1a82f5
shreya.arora <shreya.arora@searce.com>
2021-04-26 17:58:41 +0530

    error handling for add index api

--- frontend/js/services/Action.service.js
+++ frontend/js/services/Action.service.js
@@ -356,6 +356,10 @@ const Actions = (() => {
         Store.updatePrimaryKeys(res);
         Store.updateTableData("reportTabContent", res);
       }
+      else {
+        res = await res.text();
+        showSnackbar(res, " redBg");
+      }
     },
 
     createNewSecIndex: (id) => {
