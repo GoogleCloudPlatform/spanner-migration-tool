ff69092e6a3fc197f5099d77e439fcbc38fdb927
charvisingla <charvisingla@google.com>
2021-10-11 19:54:29 +0000

    Used common ddl conversion code for dynamodb.

--- sources/dynamodb/toddl.go
+++ sources/dynamodb/toddl.go
@@ -15,67 +15,21 @@
 package dynamodb
 
 import (
-	"fmt"
-	"strconv"
-	"unicode"
-
 	"github.com/cloudspannerecosystem/harbourbridge/internal"
 	"github.com/cloudspannerecosystem/harbourbridge/schema"
 	"github.com/cloudspannerecosystem/harbourbridge/spanner/ddl"
 )
 
-// schemaToDDL performs schema conversion from the source DB schema to
-// Spanner. It uses the source schema in conv.SrcSchema, and writes
-// the Spanner schema to conv.SpSchema.
-func schemaToDDL(conv *internal.Conv) error {
-	for _, srcTable := range conv.SrcSchema {
-		spTableName, err := internal.GetSpannerTable(conv, srcTable.Name)
-		if err != nil {
-			conv.Unexpected(fmt.Sprintf("Couldn't map source table %s to Spanner: %s", srcTable.Name, err))
-			continue
-		}
-		var spColNames []string
-		spColDef := make(map[string]ddl.ColumnDef)
-		conv.Issues[srcTable.Name] = make(map[string][]internal.SchemaIssue)
-		// Iterate over columns using ColNames order.
-		for _, srcColName := range srcTable.ColNames {
-			srcCol := srcTable.ColDefs[srcColName]
-			colName, err := internal.GetSpannerCol(conv, srcTable.Name, srcCol.Name, false)
-			if err != nil {
-				conv.Unexpected(fmt.Sprintf("Couldn't map source column %s of table %s to Spanner: %s", srcTable.Name, srcCol.Name, err))
-				continue
-			}
-			spColNames = append(spColNames, colName)
-			ty, issues := toSpannerType(conv, srcCol.Type.Name, srcCol.Type.Mods)
-
-			if len(issues) > 0 {
-				conv.Issues[srcTable.Name][srcCol.Name] = issues
-			}
-			spColDef[colName] = ddl.ColumnDef{
-				Name:    colName,
-				T:       ty,
-				NotNull: srcCol.NotNull,
-				Comment: "From: " + quoteIfNeeded(srcCol.Name) + " " + srcCol.Type.Print(),
-			}
-		}
-		comment := "Spanner schema for source table " + quoteIfNeeded(srcTable.Name)
-		conv.SpSchema[spTableName] = ddl.CreateTable{
-			Name:     spTableName,
-			ColNames: spColNames,
-			ColDefs:  spColDef,
-			Pks:      cvtPrimaryKeys(conv, srcTable.Name, srcTable.PrimaryKeys),
-			Indexes:  cvtIndexes(conv, spTableName, srcTable.Name, srcTable.Indexes),
-			Comment:  comment}
-	}
-	return nil
+// ToDdl implementation for DynamoDB
+type ToDdlImpl struct {
 }
 
 // toSpannerType maps a scalar source schema type (defined by id and
 // mods) into a Spanner type. This is the core source-to-Spanner type
 // mapping.  toSpannerType returns the Spanner type and a list of type
 // conversion issues encountered.
-func toSpannerType(conv *internal.Conv, id string, mods []int64) (ddl.Type, []internal.SchemaIssue) {
-	switch id {
+func (tdi ToDdlImpl) ToSpannerType(conv *internal.Conv, columnType schema.Type) (ddl.Type, []internal.SchemaIssue) {
+	switch columnType.Name {
 	case typeNumber:
 		return ddl.Type{Name: ddl.Numeric}, nil
 	case typeNumberString, typeString, typeList, typeMap:
@@ -93,51 +47,4 @@ func toSpannerType(conv *internal.Conv, id string, mods []int64) (ddl.Type, []in
 	default:
 		return ddl.Type{Name: ddl.String, Len: ddl.MaxLength}, []internal.SchemaIssue{internal.NoGoodType}
 	}
-}
-
-func quoteIfNeeded(s string) string {
-	for _, r := range s {
-		if unicode.IsLetter(r) || unicode.IsDigit(r) || unicode.IsPunct(r) {
-			continue
-		}
-		return strconv.Quote(s)
-	}
-	return s
-}
-
-func cvtPrimaryKeys(conv *internal.Conv, srcTable string, srcKeys []schema.Key) []ddl.IndexKey {
-	var spKeys []ddl.IndexKey
-	for _, k := range srcKeys {
-		spCol, err := internal.GetSpannerCol(conv, srcTable, k.Column, true)
-		if err != nil {
-			conv.Unexpected(fmt.Sprintf("Can't map key for table %s", srcTable))
-			continue
-		}
-		spKeys = append(spKeys, ddl.IndexKey{Col: spCol, Desc: k.Desc})
-	}
-	return spKeys
-}
-
-func cvtIndexes(conv *internal.Conv, spTableName string, srcTable string, srcIndexes []schema.Index) []ddl.CreateIndex {
-	var spIndexes []ddl.CreateIndex
-	for _, srcIndex := range srcIndexes {
-		var spKeys []ddl.IndexKey
-		for _, k := range srcIndex.Keys {
-			spCol, err := internal.GetSpannerCol(conv, srcTable, k.Column, true)
-			if err != nil {
-				conv.Unexpected(fmt.Sprintf("Can't map index key column name for table %s, column %s", srcTable, k.Column))
-				continue
-			}
-			spKeys = append(spKeys, ddl.IndexKey{Col: spCol, Desc: k.Desc})
-		}
-		spIndexName := internal.ToSpannerIndexName(conv, srcIndex.Name)
-		spIndex := ddl.CreateIndex{
-			Name:   spIndexName,
-			Table:  spTableName,
-			Unique: srcIndex.Unique,
-			Keys:   spKeys,
-		}
-		spIndexes = append(spIndexes, spIndex)
-	}
-	return spIndexes
 }
