11a94cc231d502eab63bcb02a59e8bbc514880e3
Isabella Pham <ipham@google.com>
2021-11-08 16:35:44 +0000

    rebase

--- main.go
+++ main.go
@@ -74,16 +74,6 @@ func setupGlobalFlags() {
 >>>>>>> 4eb7efa (removed dump file location flag)
 }
 
-<<<<<<< HEAD
-func didSetVerboseTwice() bool {
-	numTimesSet := 0
-	flag.Visit(func(f *flag.Flag) {
-		if f.Name == "v" || f.Name == "verbose" {
-			numTimesSet++
-		}
-	})
-	return numTimesSet > 1
-=======
 func didSetVerboseTwice() bool{
 	numTimesSet := 0
 	flag.Visit(func(f *flag.Flag) {
@@ -92,7 +82,6 @@ func didSetVerboseTwice() bool{
         }
     })
     return numTimesSet > 1
->>>>>>> 16092e4 (added -verbose flag to meet gcloud criteria (#205))
 }
 
 func usage() {
@@ -141,11 +130,7 @@ func main() {
 	if didSetVerboseTwice() {
 		panic(fmt.Errorf("cannot set both -v and -verbose flags"))
 	}
-<<<<<<< HEAD
-
-=======
 	
->>>>>>> 16092e4 (added -verbose flag to meet gcloud criteria (#205))
 	internal.VerboseInit(verbose)
 	if schemaOnly && dataOnly {
 		panic(fmt.Errorf("can't use both schema-only and data-only modes at once"))
