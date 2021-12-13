240761d9e13c48dfe161555cf7cdaeb9f9f85951
shellymathew98 <shelly.mathew@searce.com>
2021-12-13 12:51:24 +0530

    mssql - code cleaning

--- sources/sqlserver/data.go
+++ sources/sqlserver/data.go
@@ -12,10 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// **************** TODO *************
-// this data file is for postgres database
-// put here so we do not get error in infoschema
-// ***************
 package sqlserver
 
 import (
