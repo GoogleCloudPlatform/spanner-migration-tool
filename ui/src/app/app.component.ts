4e497ace505047e792bf2a5efed5d46861c4f73c
anupam-searce <anupam.bhattacharyya@searce.com>
2022-03-04 20:52:21 +0530

    add full width progress bar

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ui';
+  showProgress = false;
+  ngOnInit() {
+    setInterval(
+      () => { this.showProgress = !this.showProgress; }
+      , 5000);
+  }
 }
+
