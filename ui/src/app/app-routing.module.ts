798ae149b1b9ddc089a44f3f5de34b56e71b6e56
anupam-searce <anupam.bhattacharyya@searce.com>
2022-03-04 12:47:59 +0530

    routes created

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DirectConnectionComponent } from './direct-connection/direct-connection.component';
+import { HomeComponent } from './home/home.component';
+import { InstructionComponent } from './instruction/instruction.component';
+import { LoadDumpComponent } from './load-dump/load-dump.component';
+import { LoadSessionComponent } from './load-session/load-session.component';
+import { SourceSelectionComponent } from './source-selection/source-selection.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'source',
+    component: SourceSelectionComponent
+  },
+  {
+    path: 'workspace',
+    component: WorkspaceComponent,
+    children: [],
+  },
+  {
+    path: 'instruction',
+    component: InstructionComponent,
+    children: [],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
