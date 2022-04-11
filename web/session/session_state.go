72df2e16dad656f5ad86b8845b782bdca1f6d5b2
anupam-searce <anupam.bhattacharyya@searce.com>
2022-04-11 15:48:10 +0530

    session code refactoring

--- web/session/session_state.go
+++ web/session/session_state.go
@@ -1,22 +1,10 @@
 package session
 
 import (
-	"database/sql"
 	"sync"
-
-	"github.com/cloudspannerecosystem/harbourbridge/internal"
 )
 
 var once sync.Once
-
-// SessionState stores information for the current migration session.
-type SessionState struct {
-	SourceDB    *sql.DB        // Connection to source database in case of direct connection
-	DbName      string         // Name of source database
-	Driver      string         // Name of HarbourBridge driver in use
-	Conv        *internal.Conv // Current conversion state
-	SessionFile string         // Path to session file
-}
 
 // sessionState maintains the current state of the session, and is used to
 // track state from one request to the next. Session state is global:
--- web/session/types.go
+++ web/session/types.go
@@ -1,6 +1,7 @@
 package session
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/cloudspannerecosystem/harbourbridge/internal"
@@ -27,4 +28,13 @@ type SessionMetadata struct {
 type ConvWithMetadata struct {
 	SessionMetadata
 	internal.Conv
+}
+
+// SessionState stores information for the current migration session.
+type SessionState struct {
+	SourceDB    *sql.DB        // Connection to source database in case of direct connection
+	DbName      string         // Name of source database
+	Driver      string         // Name of HarbourBridge driver in use
+	Conv        *internal.Conv // Current conversion state
+	SessionFile string         // Path to session file
 }
