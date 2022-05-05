487e349d04e722826a499a3d8b0dfdd7540e54ce
pratikdhanavesearce <pratik.dhanave@searce.com>
2022-05-05 19:50:09 +0530

    test for primarykey api

--- webv2/primary_test.go
+++ webv2/primary_test.go
@@ -223,3 +223,79 @@ func TestRemovePrimaryKey(t *testing.T) {
 
 	assert.Equal(t, expected, res)
 }
+
+func TestPrimarykey(t *testing.T) {
+
+	sessionState := session.GetSessionState()
+
+	sessionState.Driver = constants.MYSQL
+
+	c := &internal.Conv{
+
+		SpSchema: map[string]ddl.CreateTable{
+			"film_actor": ddl.CreateTable{
+				Name:     "film_actor",
+				ColNames: []string{"film_id", "actor_id", "last_update"},
+				ColDefs: map[string]ddl.ColumnDef{
+					"film_id":     ddl.ColumnDef{Name: "film_id", T: ddl.Type{Name: ddl.String, Len: ddl.MaxLength}, Id: 1},
+					"actor_id":    ddl.ColumnDef{Name: "actor_id", T: ddl.Type{Name: ddl.String, Len: ddl.MaxLength}, Id: 2},
+					"last_update": ddl.ColumnDef{Name: "last_update", T: ddl.Type{Name: ddl.String, Len: ddl.MaxLength}, Id: 3},
+				},
+				Pks:          []ddl.IndexKey{ddl.IndexKey{Col: "film_id", Order: 1, Desc: true}, ddl.IndexKey{Col: "actor_id", Order: 2, Desc: true}},
+				Id:           1,
+				PrimaryKeyId: 1,
+			}},
+	}
+
+	sessionState.Conv = c
+
+	tc := []struct {
+		name        string
+		input       PrimaryKeyRequest
+		statusCode  int
+		res         PrimaryKeyResponse
+		expectedRes PrimaryKeyResponse
+	}{
+		{
+			name: "Table Id Not found",
+			input: PrimaryKeyRequest{
+				TableId:      99,
+				PrimaryKeyId: 99,
+				Columns:      []Column{Column{ColumnId: 1, ColName: "film_id", Desc: true, Order: 1}, Column{ColumnId: 2, ColName: "actor_id", Desc: true, Order: 2}},
+			},
+			statusCode: http.StatusNotFound,
+		},
+		{
+			name: "Column are empty",
+			input: PrimaryKeyRequest{
+				TableId:      1,
+				PrimaryKeyId: 1,
+				Columns:      []Column{}},
+			statusCode: http.StatusBadRequest,
+		},
+	}
+
+	for _, tt := range tc {
+
+		inputBytes, err := json.Marshal(tt.input)
+		if err != nil {
+			t.Fatal(err)
+		}
+		buffer := bytes.NewBuffer(inputBytes)
+
+		req, err := http.NewRequest("POST", "/primarykey", buffer)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		rr := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(primaryKey)
+		handler.ServeHTTP(rr, req)
+
+		json.Unmarshal(rr.Body.Bytes(), &tt.res)
+
+		assert.Equal(t, tt.statusCode, rr.Code)
+	}
+}
