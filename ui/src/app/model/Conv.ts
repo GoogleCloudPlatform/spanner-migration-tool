d5c10d7df490cf69bf0f14a3cddb51aa64da5438
anupam-searce <anupam.bhattacharyya@searce.com>
2022-05-09 12:52:07 +0530

    remove unused properties and imports

--- ui/src/app/model/Conv.ts
+++ ui/src/app/model/Conv.ts
@@ -1,5 +1,4 @@
 export default interface IConv {
-  mode: number
   SpSchema: Record<string, ICreateTable>
   SyntheticPKeys: Record<string, ISyntheticPKey>
   SrcSchema: Record<string, ITable>
@@ -128,8 +127,4 @@ export interface IType {
 export interface ISyntheticPKey {
   Col: string
   Sequence: Number
-}
-export interface IGlobalType {
-  source: string
-  destination: string
 }
