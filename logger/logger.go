3f7c9025221c89faac882c0052f6866e2c116f65
Manit Gupta <manitgupta@google.com>
2022-07-11 09:57:59 +0530

    add comments explaining logger creation

--- logger/logger.go
+++ logger/logger.go
@@ -12,18 +12,27 @@ const LOG_FILE_NAME = "harbourbridge.log"
 var Log *zap.Logger
 
 func InitializeLogger(inputLogLevel string) error {
+	// create zapper encoding config object
 	config := zap.NewProductionEncoderConfig()
+	// set logging timestamp format
 	config.EncodeTime = zapcore.ISO8601TimeEncoder
+	// create encoder for logs that are written to file
 	fileEncoder := zapcore.NewJSONEncoder(config)
+	// create encoder for logs that are written to console
+	// we create two encoders because we want to write human readable logs to console and
+	// JSON parsable logs to the file
 	consoleEncoder := zapcore.NewConsoleEncoder(config)
+	// specify log file.
 	logFile, _ := os.OpenFile(LOG_FILE_NAME, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	writer := zapcore.AddSync(logFile)
+	// create and set the log level from the user input
 	zapLogLevel := new(zapcore.Level)
 	err := zapLogLevel.Set(inputLogLevel)
 	if err != nil {
 		return err
 	}
 	logLevel := zap.NewAtomicLevelAt(*zapLogLevel)
+	// create the logger
 	core := zapcore.NewTee(
 		zapcore.NewCore(fileEncoder, writer, logLevel),
 		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), logLevel),
