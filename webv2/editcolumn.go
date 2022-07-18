8066791cfe0484ec7b410b6d20f6fac64f1f6af2
pratikdhanavesearce <pratik.dhanave@searce.com>
2022-07-18 19:03:42 +0530

    wip

---
+++ webv2/editcolumn.go
@@ -0,0 +1,195 @@
+package webv2
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	helpers "github.com/cloudspannerecosystem/harbourbridge/webv2/helpers"
+	"github.com/cloudspannerecosystem/harbourbridge/webv2/session"
+)
+
+// Actions to be performed on a column.
+// (1) Removed: true/false
+// (2) Rename: New name or empty string
+// (3) PK: "ADDED", "REMOVED" or ""
+// (4) NotNull: "ADDED", "REMOVED" or ""
+// (5) ToType: New type or empty string
+type updateCol struct {
+	Add     bool   `json:"Add"`
+	Removed bool   `json:"Removed"`
+	Rename  string `json:"Rename"`
+	PK      string `json:"PK"`
+	NotNull string `json:"NotNull"`
+	ToType  string `json:"ToType"`
+}
+
+type updateTable struct {
+	UpdateCols map[string]updateCol `json:"UpdateCols"`
+}
+
+// updateTableSchema updates the Spanner schema.
+// Following actions can be performed on a specified table:
+// (1) Remove column
+// (2) Rename column
+// (3) Add or Remove Primary Key
+// (4) Add or Remove NotNull constraint
+// (5) Update Spanner type
+func updateTableSchema(w http.ResponseWriter, r *http.Request) {
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Body Read Error : %v", err), http.StatusInternalServerError)
+		return
+	}
+	var t updateTable
+
+	table := r.FormValue("table")
+
+	fmt.Println("updateTableSchema getting called")
+
+	err = json.Unmarshal(reqBody, &t)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Request Body parse error : %v", err), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println("updateTable :", t)
+
+	sessionState := session.GetSessionState()
+	srcTableName := sessionState.Conv.ToSource[table].Name
+
+	for colName, v := range t.UpdateCols {
+
+		if v.Removed {
+
+			/*
+				status, err := canRemoveColumn(colName, table)
+
+				if err != nil {
+					http.Error(w, fmt.Sprintf("%v", err), status)
+					return
+				}
+
+			*/
+			addColumn(table, colName, srcTableName)
+
+			continue
+		}
+
+		if v.Removed {
+
+			/*
+				status, err := canRemoveColumn(colName, table)
+
+				if err != nil {
+					http.Error(w, fmt.Sprintf("%v", err), status)
+					return
+				}
+
+			*/
+			removeColumn(table, colName, srcTableName)
+
+			continue
+		}
+
+		if v.Rename != "" && v.Rename != colName {
+
+			renameColumn(v.Rename, table, colName, srcTableName)
+			colName = v.Rename
+		}
+
+		if v.ToType != "" {
+
+			typeChange, err := isTypeChanged(v.ToType, table, colName, srcTableName)
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			if typeChange {
+
+				/*
+					if status, err := canRenameOrChangeType(colName, table); err != nil {
+
+						http.Error(w, fmt.Sprintf("%v", err), status)
+						return
+					}
+				*/
+				updateType(v.ToType, table, colName, srcTableName, w)
+			}
+		}
+
+		if v.NotNull != "" {
+			updateNotNull(v.NotNull, table, colName)
+		}
+	}
+
+	helpers.UpdateSessionFile()
+
+	convm := session.ConvWithMetadata{
+		SessionMetadata: sessionState.SessionMetadata,
+		Conv:            *sessionState.Conv,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(convm)
+}
+
+func canRenameOrChangeType(colName, table string) (int, error) {
+
+	sessionState := session.GetSessionState()
+
+	isPartOfPK := isPartOfPK(colName, table)
+
+	isParent, childSchema := isParent(table)
+
+	isChild := sessionState.Conv.SpSchema[table].Parent != ""
+
+	if isPartOfPK && (isParent || isChild) {
+		return http.StatusBadRequest, fmt.Errorf("column : '%s' in table : '%s' is part of parent-child relation with schema : '%s'", colName, table, childSchema)
+	}
+
+	if isPartOfSecondaryIndex, indexName := isPartOfSecondaryIndex(colName, table); isPartOfSecondaryIndex {
+		return http.StatusPreconditionFailed, fmt.Errorf("column : '%s' in table : '%s' is part of secondary index : '%s', remove secondary index before making the update",
+			colName, table, indexName)
+	}
+
+	isPartOfFK := isPartOfFK(colName, table)
+
+	isReferencedByFK, relationTable := isReferencedByFK(colName, table)
+
+	if isPartOfFK || isReferencedByFK {
+		if isReferencedByFK {
+			return http.StatusPreconditionFailed, fmt.Errorf("column : '%s' in table : '%s' is part of foreign key relation with table : '%s', remove foreign key constraint before making the update",
+				colName, table, relationTable)
+		}
+		return http.StatusPreconditionFailed, fmt.Errorf("column : '%s' in table : '%s' is part of foreign keys, remove foreign key constraint before making the update",
+			colName, table)
+	}
+
+	return http.StatusOK, nil
+}
+
+func canRemoveColumn(colName, table string) (int, error) {
+
+	if isPartOfPK := isPartOfPK(colName, table); isPartOfPK {
+		return http.StatusBadRequest, fmt.Errorf("column is part of primary key")
+	}
+
+	if isPartOfSecondaryIndex, _ := isPartOfSecondaryIndex(colName, table); isPartOfSecondaryIndex {
+		return http.StatusPreconditionFailed, fmt.Errorf("column is part of secondary index, remove secondary index before making the update")
+	}
+
+	isPartOfFK := isPartOfFK(colName, table)
+
+	isReferencedByFK, _ := isReferencedByFK(colName, table)
+
+	if isPartOfFK || isReferencedByFK {
+		return http.StatusPreconditionFailed, fmt.Errorf("column is part of foreign key relation, remove foreign key constraint before making the update")
+	}
+
+	return http.StatusOK, nil
+}
---
+++ webv2/removeColumn.go
@@ -0,0 +1,305 @@
+package webv2
+
+import (
+	"fmt"
+
+	"github.com/cloudspannerecosystem/harbourbridge/spanner/ddl"
+	"github.com/cloudspannerecosystem/harbourbridge/webv2/session"
+)
+
+func removeColumn(table string, colName string, srcTableName string) {
+
+	sessionState := session.GetSessionState()
+
+	sp := sessionState.Conv.SpSchema[table]
+
+	// step I
+	sp = removeSpannerColDefs(sp, colName)
+
+	// step II
+	sp = removeSpannerPK(sp, colName)
+
+	// step III
+	sp = removeSpannerSecondaryIndex(sp, colName)
+
+	// step IV
+	sp = removeSpannerForeignkeyColumns(sp, colName)
+
+	// step V
+	sp = removeSpannerForeignkeyReferColumns(sp, colName)
+
+	// step VI
+	sp = removeSpannerColNames(sp, colName)
+
+	// step VII
+	removeSpannerSchemaIssue(table, colName)
+
+	// step VIII
+	removeToSpannerToSource(table, colName)
+	sessionState.Conv.SpSchema[table] = sp
+
+	// update foreignKey relationship Table column names
+	for i, _ := range sp.Fks {
+
+		fmt.Println("update foreignKey Table column names")
+
+		relationTable := sp.Fks[i].ReferTable
+
+		relationTableSp := sessionState.Conv.SpSchema[relationTable]
+
+		relationTableSp = removeSpannerColNames(relationTableSp, colName)
+		relationTableSp = removeSpannerColDefs(relationTableSp, colName)
+		relationTableSp = removeSpannerPK(relationTableSp, colName)
+		relationTableSp = removeSpannerSecondaryIndex(relationTableSp, colName)
+		relationTableSp = removeSpannerForeignkeyColumns(relationTableSp, colName)
+		relationTableSp = removeSpannerForeignkeyReferColumns(relationTableSp, colName)
+
+		//todo
+		removeSpannerSchemaIssue(relationTable, colName)
+
+		removeToSpannerToSource(relationTable, colName)
+
+		sessionState.Conv.SpSchema[relationTable] = relationTableSp
+
+	}
+
+	// update interleave table relation
+	isParent, childSchema := isParent(table)
+
+	if isParent {
+		fmt.Println("yes", table, "is parent table")
+
+		childSchemaSp := sessionState.Conv.SpSchema[childSchema]
+
+		childSchemaSp = removeSpannerColNames(childSchemaSp, colName)
+		childSchemaSp = removeSpannerColDefs(childSchemaSp, colName)
+		childSchemaSp = removeSpannerPK(childSchemaSp, colName)
+		childSchemaSp = removeSpannerSecondaryIndex(childSchemaSp, colName)
+		childSchemaSp = removeSpannerForeignkeyColumns(childSchemaSp, colName)
+		childSchemaSp = removeSpannerForeignkeyReferColumns(childSchemaSp, colName)
+
+		//todo
+		removeSpannerSchemaIssue(childSchema, colName)
+
+		removeToSpannerToSource(childSchema, colName)
+
+		sessionState.Conv.SpSchema[childSchema] = childSchemaSp
+
+	}
+
+	isChild := sessionState.Conv.SpSchema[table].Parent
+
+	if isChild != "" {
+
+		childSchemaSp := sessionState.Conv.SpSchema[isChild]
+
+		childSchemaSp = removeSpannerColNames(childSchemaSp, colName)
+		childSchemaSp = removeSpannerColDefs(childSchemaSp, colName)
+		childSchemaSp = removeSpannerPK(childSchemaSp, colName)
+		childSchemaSp = removeSpannerSecondaryIndex(childSchemaSp, colName)
+		childSchemaSp = removeSpannerForeignkeyColumns(childSchemaSp, colName)
+		childSchemaSp = removeSpannerForeignkeyReferColumns(childSchemaSp, colName)
+
+		//todo
+		removeSpannerSchemaIssue(isChild, colName)
+		removeToSpannerToSource(isChild, colName)
+
+		sessionState.Conv.SpSchema[isChild] = childSchemaSp
+	}
+}
+
+func removeSpannerColNames(sp ddl.CreateTable, colName string) ddl.CreateTable {
+
+	// step VI
+	// remove sp.ColNames
+
+	fmt.Println("")
+	fmt.Println("step VI")
+
+	for i, col := range sp.ColNames {
+		if col == colName {
+			sp.ColNames = remove(sp.ColNames, i)
+			break
+		}
+	}
+
+	fmt.Println("removing sp.ColNames : ", colName)
+
+	delete(sp.ColDefs, colName)
+
+	fmt.Println("removed sp.ColNames : ", sp.ColDefs)
+
+	return sp
+
+}
+
+func removeSpannerPK(sp ddl.CreateTable, colName string) ddl.CreateTable {
+
+	// step II
+	// remove sp.Pks
+
+	fmt.Println("")
+	fmt.Println("step II")
+
+	for i, pk := range sp.Pks {
+		if pk.Col == colName {
+
+			fmt.Println("removing sp.Pks : ", i)
+
+			sp.Pks = removePk(sp.Pks, i)
+
+			fmt.Println("removed sp.Pks : ", sp.Pks)
+
+			break
+		}
+	}
+
+	return sp
+}
+
+func removeSpannerColDefs(sp ddl.CreateTable, colName string) ddl.CreateTable {
+
+	// step I
+	// remove sp.ColDefs
+
+	fmt.Println("")
+	fmt.Println("step I")
+
+	if _, found := sp.ColDefs[colName]; found {
+
+		fmt.Println("removing sp.ColDefs : ")
+
+		delete(sp.ColDefs, colName)
+
+		fmt.Println("removed sp.ColDefs :", sp.ColDefs)
+
+	}
+
+	return sp
+}
+
+func removeSpannerSecondaryIndex(sp ddl.CreateTable, colName string) ddl.CreateTable {
+
+	// step III
+	// update sp.Indexes
+	fmt.Println("")
+	fmt.Println("step III")
+
+	for i, index := range sp.Indexes {
+		for j, key := range index.Keys {
+			if key.Col == colName {
+
+				fmt.Println("removing sp.Indexes[i].Keys[j].Col : ")
+
+				sp.Indexes[i].Keys = removeSecondaryIndexKey(sp.Indexes[i].Keys, j)
+
+				fmt.Println("removed sp.Indexes[i].Keys[j].Col : ", sp.Indexes[i].Keys[j])
+
+				break
+			}
+		}
+	}
+
+	return sp
+}
+
+func removeSecondaryIndexKey(slice []ddl.IndexKey, s int) []ddl.IndexKey {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func removeSpannerSchemaIssue(table string, colName string) {
+
+	// step VII
+	// remove sessionState.Conv.Issues
+
+	fmt.Println("")
+	fmt.Println("step VII")
+
+	sessionState := session.GetSessionState()
+
+	if sessionState.Conv.Issues != nil {
+
+		if sessionState.Conv.Issues[table] != nil && sessionState.Conv.Issues[table][colName] != nil {
+
+			delete(sessionState.Conv.Issues[table], colName)
+
+		}
+
+	}
+
+}
+
+func removeToSpannerToSource(table string, colName string) {
+
+	// step VIII
+	// remove ToSpannerToSource
+
+	fmt.Println("")
+	fmt.Println("step VII")
+
+	sessionState := session.GetSessionState()
+
+	srcTableName := sessionState.Conv.ToSource[table].Name
+
+	srcColName := sessionState.Conv.ToSource[table].Cols[colName]
+	delete(sessionState.Conv.ToSource[table].Cols, colName)
+	delete(sessionState.Conv.ToSpanner[srcTableName].Cols, srcColName)
+
+}
+
+func removeSpannerForeignkeyColumns(sp ddl.CreateTable, colName string) ddl.CreateTable {
+
+	// step IV
+	// update sp.fk.Columns
+	fmt.Println("")
+	fmt.Println("step IV")
+
+	for i, fk := range sp.Fks {
+		for j, column := range fk.Columns {
+			if column == colName {
+
+				fmt.Println("removing sp.Fks[i].Columns[j] : ", sp.Fks[i].Columns[j])
+
+				sp.Fks[i].Columns = removeFkColumns(fk.Columns, j)
+
+				fmt.Println("removed sp.Fks[i].Columns[j] :", sp.Fks[i].Columns)
+
+			}
+		}
+	}
+
+	return sp
+}
+
+func removeFkColumns(slice []string, s int) []string {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func removeSpannerForeignkeyReferColumns(sp ddl.CreateTable, colName string) ddl.CreateTable {
+
+	// step IV
+	// update sp.fk.ReferColumns
+	fmt.Println("")
+	fmt.Println("step IV")
+
+	for i, fk := range sp.Fks {
+		for j, column := range fk.ReferColumns {
+
+			if column == colName {
+
+				fmt.Println(" removing sp.Fks[i].ReferColumns[j] :", sp.Fks[i].ReferColumns[j])
+
+				sp.Fks[i].ReferColumns = removeFkReferColumns(sp.Fks[i].ReferColumns, j)
+
+				fmt.Println("removed sp.Fks[i].ReferColumns[j] :", sp.Fks[i].ReferColumns)
+
+			}
+
+		}
+	}
+	return sp
+}
+
+func removeFkReferColumns(slice []string, s int) []string {
+	return append(slice[:s], slice[s+1:]...)
+}
