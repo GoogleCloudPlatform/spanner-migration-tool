39b4ed2bc479d244a4e448c61b077f63de209799
lyazii22 <lyazii.christopher@searce.com>
2022-08-11 18:49:17 +0530

    conv Audit response minimize to minimal requirement

--- webv2/session/remote_session_store.go
+++ webv2/session/remote_session_store.go
@@ -21,6 +21,7 @@ import (
 
 	"cloud.google.com/go/spanner"
 	"github.com/cloudspannerecosystem/harbourbridge/internal"
+	"github.com/cloudspannerecosystem/harbourbridge/proto/migration"
 	"google.golang.org/api/iterator"
 )
 
@@ -111,10 +112,8 @@ func (st *spannerStore) GetConvWithMetadata(ctx context.Context, versionId strin
 	}
 
 	convm.Conv = conv
-	//TBD to add or remove
-	// convm.Conv.Audit = internal.Audit{
-	// 	MigrationType: migration.MigrationData_SCHEMA_ONLY.Enum(),
-	// }
+	convm.Conv.Audit.MigrationType = migration.MigrationData_SCHEMA_ONLY.Enum()
+
 	convm.SessionMetadata = SessionMetadata{
 		SessionName:  scs.SessionName,
 		EditorName:   scs.EditorName,
