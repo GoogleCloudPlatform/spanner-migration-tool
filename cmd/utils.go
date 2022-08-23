86d551cb838ae91a685a5d1d3efbd4de95f18480
Shreya Khajanchi <khajanchi@google.com>
2022-08-23 14:53:55 +0530

    minor fixes

--- cmd/utils.go
+++ cmd/utils.go
@@ -111,7 +111,7 @@ func MigrateDatabase(ctx context.Context, targetProfile profiles.TargetProfile, 
 	defer client.Close()
 	switch v := cmd.(type) {
 	case *SchemaCmd:
-		err = migrateSchema(ctx, targetProfile, sourceProfile, ioHelper, conv, dbURI, adminClient)
+		err = migrateSchema(ctx, targetProfile, sourceProfile, ioHelper, conv, dbURI, adminClient, progress)
 	case *DataCmd:
 		bw, err = migrateData(ctx, targetProfile, sourceProfile, ioHelper, conv, dbURI, adminClient, client, v, progress)
 	case *SchemaAndDataCmd:
@@ -125,7 +125,7 @@ func MigrateDatabase(ctx context.Context, targetProfile profiles.TargetProfile, 
 }
 
 func migrateSchema(ctx context.Context, targetProfile profiles.TargetProfile, sourceProfile profiles.SourceProfile,
-	ioHelper *utils.IOStreams, conv *internal.Conv, dbURI string, adminClient *database.DatabaseAdminClient) error {
+	ioHelper *utils.IOStreams, conv *internal.Conv, dbURI string, adminClient *database.DatabaseAdminClient, progress *internal.Progress) error {
 	err := conversion.CreateOrUpdateDatabase(ctx, adminClient, dbURI, sourceProfile.Driver, targetProfile.TargetDb, conv, ioHelper.Out)
 	if err != nil {
 		err = fmt.Errorf("can't create/update database: %v", err)
