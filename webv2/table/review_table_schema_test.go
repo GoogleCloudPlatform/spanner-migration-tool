4b15e407b8a2f28711c525ebee28a21074d8c89f
lyazii22 <lyazii.christopher@searce.com>
2022-09-28 16:53:37 +0530

    Merge interleave and non-interleave test cases into a single file

--- webv2/table/review_table_schema_test.go
+++ webv2/table/review_table_schema_test.go
@@ -30,7 +30,7 @@ import (
 	"github.com/cloudspannerecosystem/harbourbridge/webv2/session"
 )
 
-func TestReviewTableSchemachangetype(t *testing.T) {
+func TestReviewTableSchemaChangeType(t *testing.T) {
 
 	tc := []struct {
 		name         string
@@ -162,7 +162,7 @@ func TestReviewTableSchemachangetype(t *testing.T) {
 	}
 }
 
-func TestReviewTableSchemaAddsuccess(t *testing.T) {
+func TestReviewTableSchemaAddSuccess(t *testing.T) {
 
 	tc := []struct {
 		name         string
@@ -424,6 +424,134 @@ func TestReviewTableSchemaRemove(t *testing.T) {
 			},
 		},
 	}
+	for _, tc := range tc {
+
+		sessionState := session.GetSessionState()
+		sessionState.Conv = tc.conv
+		sessionState.Driver = constants.MYSQL
+
+		payload := tc.payload
+
+		req, err := http.NewRequest("POST", "/typemap/reviewtableschema?table="+tc.table, strings.NewReader(payload))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+
+		rr := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(ReviewTableSchema)
+
+		handler.ServeHTTP(rr, req)
+
+		res := ReviewTableSchemaResponse{}
+
+		json.Unmarshal(rr.Body.Bytes(), &res)
+
+		if status := rr.Code; int64(status) != tc.statusCode {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, tc.statusCode)
+		}
+
+		expectedddl := GetSpannerTableDDL(tc.expectedConv.SpSchema[tc.table])
+
+		if tc.statusCode == http.StatusOK {
+			assert.Equal(t, expectedddl, res.DDL)
+		}
+	}
+}
+
+func TestReviewTableSchemaInterleaved(t *testing.T) {
+
+	tc := []struct {
+		name         string
+		table        string
+		payload      string
+		statusCode   int64
+		conv         *internal.Conv
+		expectedConv *internal.Conv
+	}{
+
+		{
+			name:  "Test rename success",
+			table: "t1",
+			payload: `
+    {
+      "UpdateCols":{
+		"a": { "Rename": "aa" }
+	}
+    }`,
+			statusCode: http.StatusOK,
+			conv: &internal.Conv{
+				SpSchema: map[string]ddl.CreateTable{
+					"t1": {
+						Name:     "t1",
+						ColNames: []string{"a", "b", "c"},
+						ColDefs: map[string]ddl.ColumnDef{
+							"a": {Name: "a", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"b": {Name: "b", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"c": {Name: "c", T: ddl.Type{Name: ddl.String, Len: ddl.MaxLength}, NotNull: true},
+						},
+						Pks:    []ddl.IndexKey{{Col: "a", Desc: false}, {Col: "b", Desc: false}},
+						Parent: "t2",
+					},
+					"t2": {
+						Name:     "t2",
+						ColNames: []string{"a", "b", "c"},
+						ColDefs: map[string]ddl.ColumnDef{
+							"a": {Name: "a", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"b": {Name: "b", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"c": {Name: "c", T: ddl.Type{Name: ddl.String, Len: ddl.MaxLength}, NotNull: true},
+						},
+						Pks: []ddl.IndexKey{{Col: "a", Desc: false}},
+					},
+				},
+				ToSource: map[string]internal.NameAndCols{
+					"t1": {Name: "t1", Cols: map[string]string{"a": "a", "b": "b", "c": "c"}},
+					"t2": {Name: "t2", Cols: map[string]string{"a": "a", "b": "b", "c": "c"}},
+				},
+				ToSpanner: map[string]internal.NameAndCols{
+					"t1": {Name: "t2", Cols: map[string]string{"a": "a", "b": "b", "c": "c"}},
+					"t2": {Name: "t2", Cols: map[string]string{"a": "a", "b": "b", "c": "c"}},
+				},
+			},
+			expectedConv: &internal.Conv{
+				SpSchema: map[string]ddl.CreateTable{
+					"t1": {
+						Name:     "t1",
+						ColNames: []string{"aa", "b", "c"},
+						ColDefs: map[string]ddl.ColumnDef{
+							"aa": {Name: "aa", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"b":  {Name: "b", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"c":  {Name: "c", T: ddl.Type{Name: ddl.String, Len: ddl.MaxLength}, NotNull: true},
+						},
+						Pks:    []ddl.IndexKey{{Col: "aa", Desc: false}, {Col: "b", Desc: false}},
+						Parent: "t2",
+					},
+					"t2": {
+						Name:     "t2",
+						ColNames: []string{"aa", "b", "c"},
+						ColDefs: map[string]ddl.ColumnDef{
+							"aa": {Name: "aa", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"b":  {Name: "b", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"c":  {Name: "c", T: ddl.Type{Name: ddl.String, Len: ddl.MaxLength}, NotNull: true},
+						},
+						Pks: []ddl.IndexKey{{Col: "aa", Desc: false}},
+					},
+				},
+				ToSource: map[string]internal.NameAndCols{
+					"t1": {Name: "t1", Cols: map[string]string{"aa": "a", "b": "b", "c": "c"}},
+					"t2": {Name: "t2", Cols: map[string]string{"aa": "a", "b": "b", "c": "c"}},
+				},
+				ToSpanner: map[string]internal.NameAndCols{
+					"t1": {Name: "t2", Cols: map[string]string{"a": "aa", "b": "b", "c": "c"}},
+					"t2": {Name: "t2", Cols: map[string]string{"a": "aa", "b": "b", "c": "c"}},
+				},
+			},
+		},
+	}
+
 	for _, tc := range tc {
 
 		sessionState := session.GetSessionState()
--- webv2/table/update_table_schema_test.go
+++ webv2/table/update_table_schema_test.go
@@ -31,7 +31,7 @@ import (
 	"github.com/cloudspannerecosystem/harbourbridge/webv2/session"
 )
 
-func TestUpdateTableSchemaV2(t *testing.T) {
+func TestUpdateTableSchema(t *testing.T) {
 
 	tc := []struct {
 		name         string
@@ -188,7 +188,7 @@ func TestUpdateTableSchemaV2(t *testing.T) {
 	}
 }
 
-func TestAddUpdateTableSchemaV2(t *testing.T) {
+func TestAddUpdateTableSchema(t *testing.T) {
 
 	tc := []struct {
 		name         string
@@ -316,7 +316,7 @@ func TestAddUpdateTableSchemaV2(t *testing.T) {
 
 }
 
-func TestChangetypeUpdateTableSchemaV2(t *testing.T) {
+func TestChangeTypeUpdateTableSchema(t *testing.T) {
 
 	tc := []struct {
 		name         string
@@ -431,6 +431,133 @@ func TestChangetypeUpdateTableSchemaV2(t *testing.T) {
 		json.Unmarshal(rr.Body.Bytes(), &res)
 
 		log.Println("res :", res.SpSchema["t1"])
+
+		if status := rr.Code; int64(status) != tc.statusCode {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, tc.statusCode)
+		}
+
+		if tc.statusCode == http.StatusOK {
+			assert.Equal(t, tc.expectedConv, res)
+		}
+	}
+}
+
+func TestUpdateTableSchemaInterleaved(t *testing.T) {
+
+	tc := []struct {
+		name         string
+		table        string
+		payload      string
+		statusCode   int64
+		conv         *internal.Conv
+		expectedConv *internal.Conv
+	}{
+
+		{
+			name:  "Test rename success",
+			table: "t1",
+			payload: `
+    {
+      "UpdateCols":{
+		"a": { "Rename": "aa" }
+	}
+    }`,
+			statusCode: http.StatusOK,
+			conv: &internal.Conv{
+				SpSchema: map[string]ddl.CreateTable{
+					"t1": {
+						Name:     "t1",
+						ColNames: []string{"a", "b", "c"},
+						ColDefs: map[string]ddl.ColumnDef{
+							"a": {Name: "a", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"b": {Name: "b", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"c": {Name: "c", T: ddl.Type{Name: ddl.String, Len: ddl.MaxLength}, NotNull: true},
+						},
+						Pks:    []ddl.IndexKey{{Col: "a", Desc: false}, {Col: "b", Desc: false}},
+						Parent: "t2",
+					},
+					"t2": {
+						Name:     "t2",
+						ColNames: []string{"a", "b", "c"},
+						ColDefs: map[string]ddl.ColumnDef{
+							"a": {Name: "a", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"b": {Name: "b", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"c": {Name: "c", T: ddl.Type{Name: ddl.String, Len: ddl.MaxLength}, NotNull: true},
+						},
+						Pks: []ddl.IndexKey{{Col: "a", Desc: false}},
+					},
+				},
+				ToSource: map[string]internal.NameAndCols{
+					"t1": {Name: "t1", Cols: map[string]string{"a": "a", "b": "b", "c": "c"}},
+					"t2": {Name: "t2", Cols: map[string]string{"a": "a", "b": "b", "c": "c"}},
+				},
+				ToSpanner: map[string]internal.NameAndCols{
+					"t1": {Name: "t2", Cols: map[string]string{"a": "a", "b": "b", "c": "c"}},
+					"t2": {Name: "t2", Cols: map[string]string{"a": "a", "b": "b", "c": "c"}},
+				},
+				Audit: internal.Audit{MigrationType: migration.MigrationData_SCHEMA_AND_DATA.Enum()},
+			},
+			expectedConv: &internal.Conv{
+				SpSchema: map[string]ddl.CreateTable{
+					"t1": {
+						Name:     "t1",
+						ColNames: []string{"aa", "b", "c"},
+						ColDefs: map[string]ddl.ColumnDef{
+							"aa": {Name: "aa", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"b":  {Name: "b", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"c":  {Name: "c", T: ddl.Type{Name: ddl.String, Len: ddl.MaxLength}, NotNull: true},
+						},
+						Pks:    []ddl.IndexKey{{Col: "aa", Desc: false}, {Col: "b", Desc: false}},
+						Parent: "t2",
+					},
+					"t2": {
+						Name:     "t2",
+						ColNames: []string{"aa", "b", "c"},
+						ColDefs: map[string]ddl.ColumnDef{
+							"aa": {Name: "aa", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"b":  {Name: "b", T: ddl.Type{Name: ddl.Int64}, NotNull: true},
+							"c":  {Name: "c", T: ddl.Type{Name: ddl.String, Len: ddl.MaxLength}, NotNull: true},
+						},
+						Pks: []ddl.IndexKey{{Col: "aa", Desc: false}},
+					},
+				},
+				ToSource: map[string]internal.NameAndCols{
+					"t1": {Name: "t1", Cols: map[string]string{"aa": "a", "b": "b", "c": "c"}},
+					"t2": {Name: "t2", Cols: map[string]string{"aa": "a", "b": "b", "c": "c"}},
+				},
+				ToSpanner: map[string]internal.NameAndCols{
+					"t1": {Name: "t2", Cols: map[string]string{"a": "aa", "b": "b", "c": "c"}},
+					"t2": {Name: "t2", Cols: map[string]string{"a": "aa", "b": "b", "c": "c"}},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tc {
+
+		sessionState := session.GetSessionState()
+		sessionState.Conv = tc.conv
+		sessionState.Driver = constants.MYSQL
+
+		payload := tc.payload
+
+		req, err := http.NewRequest("POST", "/typemap/table?table="+tc.table, strings.NewReader(payload))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+
+		rr := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(UpdateTableSchema)
+
+		handler.ServeHTTP(rr, req)
+
+		res := &internal.Conv{}
+
+		json.Unmarshal(rr.Body.Bytes(), &res)
 
 		if status := rr.Code; int64(status) != tc.statusCode {
 			t.Errorf("handler returned wrong status code: got %v want %v",
