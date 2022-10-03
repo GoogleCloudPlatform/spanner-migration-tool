a2fdbed982c579a6e5a2fc31b494ca19941e8ff3
lyazii22 <lyazii.christopher@searce.com>
2022-10-03 19:18:45 +0530

    Refactor and rebase column-edit backend code-base 1

---
+++ webv2/session/utility.go
@@ -0,0 +1,22 @@
+package session
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/cloudspannerecosystem/harbourbridge/common/utils"
+	"github.com/cloudspannerecosystem/harbourbridge/conversion"
+)
+
+// UpdateSessionFile updates the content of session file with
+// latest sessionState.Conv while also dumping schemas and report.
+func UpdateSessionFile() error {
+	sessionState := GetSessionState()
+
+	ioHelper := &utils.IOStreams{In: os.Stdin, Out: os.Stdout}
+	_, err := conversion.WriteConvGeneratedFiles(sessionState.Conv, sessionState.DbName, sessionState.Driver, ioHelper.BytesRead, ioHelper.Out)
+	if err != nil {
+		return fmt.Errorf("Error encountered while updating session session file %w", err)
+	}
+	return nil
+}
