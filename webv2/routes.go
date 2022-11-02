3cbafba5a544bfefb79798256a10665b4b905fa5
Shreya Khajanchi <khajanchi@google.com>
2022-11-02 19:08:49 +0530

    added foreign key progress bar

--- webv2/routes.go
+++ webv2/routes.go
@@ -96,9 +96,9 @@ func getRoutes() *mux.Router {
 	// Clean up datastream and data flow jobs
 	router.HandleFunc("/CleanUpStreamingJobs", profile.CleanUpStreamingJobs).Methods("POST")
 
-	router.PathPrefix("/").Handler(frontendStatic)
-
 	router.HandleFunc("/SetSourceDBDetailsForDump", setSourceDBDetailsForDump).Methods("POST")
 	router.HandleFunc("/SetSourceDBDetailsForDirectConnect", setSourceDBDetailsForDirectConnect).Methods("POST")
+
+	router.PathPrefix("/").Handler(frontendStatic)
 	return router
 }
