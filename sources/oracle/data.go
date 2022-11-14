f676dd786fb24f9736d1442150ed69ea43aace88
lyazii22 <lyazii.christopher@searce.com>
2022-11-14 11:48:40 +0530

    Update oracle test cases (#84)
    * oracle data migration update
    
    * Oracle data conversion unit test cases update
    
    Co-authored-by: anupam-searce <anupam.bhattacharyya@searce.com>

--- sources/oracle/data.go
+++ sources/oracle/data.go
@@ -29,6 +29,7 @@ import (
 	"github.com/cloudspannerecosystem/harbourbridge/common/constants"
 	"github.com/cloudspannerecosystem/harbourbridge/internal"
 	"github.com/cloudspannerecosystem/harbourbridge/schema"
+	"github.com/cloudspannerecosystem/harbourbridge/sources/common"
 	"github.com/cloudspannerecosystem/harbourbridge/spanner/ddl"
 )
 
@@ -60,8 +61,13 @@ func convertData(conv *internal.Conv, srcTable string, srcCols []string, srcSche
 		if vals[i] == "NULL" {
 			continue
 		}
-		spColDef, ok1 := spSchema.ColDefs[spCol]
-		srcColDef, ok2 := srcSchema.ColDefs[srcCol]
+
+		spColId := common.GetSpColIdFromName(conv, spTable, spCol)
+		srcColId, _ := internal.GetColumnIdFromName(conv, srcTable, srcCol)
+
+		spColDef, ok1 := spSchema.ColDefs[spColId]
+		srcColDef, ok2 := srcSchema.ColDefs[srcColId]
+
 		if !ok1 || !ok2 {
 			return "", []string{}, []interface{}{}, fmt.Errorf("can't find Spanner and source-db schema for col %s", spCol)
 		}
@@ -84,7 +90,7 @@ func convertData(conv *internal.Conv, srcTable string, srcCols []string, srcSche
 		aux.Sequence++
 		conv.SyntheticPKeys[spTable] = aux
 	}
-	return spTable, c, v, nil
+	return spSchema.Name, c, v, nil
 }
 
 // convScalar converts a source database string value to an
