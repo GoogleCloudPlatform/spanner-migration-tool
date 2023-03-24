8a16a75640102ed47403699fb48d1827984ad9ab
Shreya Khajanchi <khajanchi@google.com>
2023-03-24 17:37:37 +0530

    fix

--- sources/oracle/infoschema.go
+++ sources/oracle/infoschema.go
@@ -181,7 +181,7 @@ func (isi InfoSchemaImpl) GetColumns(conv *internal.Conv, table common.SchemaAnd
 	if err != nil {
 		return nil, nil, fmt.Errorf("couldn't get schema for table %s.%s: %s", table.Schema, table.Name, err)
 	}
-	cols.Close()
+	defer cols.Close()
 	colDefs := make(map[string]schema.Column)
 	var colNames []string
 	var colName, dataType string
