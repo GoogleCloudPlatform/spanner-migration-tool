58b2f92f4e83e3df921d2258f5e149834f039b1d
Annu Priya Kumari <priyaannu@google.com>
2023-08-14 18:11:28 +0530

    InfoSchemaTest file changes

--- sources/mysql/infoschema_test.go
+++ sources/mysql/infoschema_test.go
@@ -382,7 +382,7 @@ func TestProcessData_MultiCol(t *testing.T) {
 	}
 	internal.AssertSpSchema(conv, t, expectedSchema, stripSchemaComments(conv.SpSchema))
 	columnLevelIssues := map[string][]internal.SchemaIssue{
-		"c5": []internal.SchemaIssue {
+		"c48": []internal.SchemaIssue {
 			2,
 		},
 	}
