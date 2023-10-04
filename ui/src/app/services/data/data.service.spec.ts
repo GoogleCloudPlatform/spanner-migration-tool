1d0e557e3fb47b566fb7bfc9853191fa68da9cbc
Shreya Khajanchi <khajanchi@google.com>
2023-10-04 10:29:45 +0530

    added github workflow

--- ui/src/app/services/data/data.service.spec.ts
+++ ui/src/app/services/data/data.service.spec.ts
@@ -1,19 +1,39 @@
 import { TestBed } from '@angular/core/testing'
 import { HttpClientModule } from '@angular/common/http'
 import { DataService } from './data.service'
-import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { FetchService } from '../fetch/fetch.service'
+import { SnackbarService } from '../snackbar/snackbar.service'
+import { of } from 'rxjs'
+import mockIConv from 'src/mocks/conv'
+import mockSpannerConfig from 'src/mocks/spannerConfig'
 
 describe('DataService', () => {
-  let service: DataService
+    let service: DataService
+    let snackbarService: SnackbarService;
+    let fetchService: FetchService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule, MatSnackBarModule],
+    // Create spy objects for dependencies
+    const snackbarServiceSpy = jasmine.createSpyObj('SnackbarService', ['openSnackBar']);
+    const fetchServiceSpy = jasmine.createSpyObj('FetchService', ['getSpannerConfig', 'getIsOffline', 'getLastSessionDetails']);
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientModule],
+            providers: [
+                DataService,
+                { provide: SnackbarService, useValue: snackbarServiceSpy },
+                { provide: FetchService, useValue: fetchServiceSpy },
+            ],
+        })
+        fetchServiceSpy.getLastSessionDetails.and.returnValue(of(mockIConv));
+        fetchServiceSpy.getSpannerConfig.and.returnValue(of(mockSpannerConfig));
+        fetchServiceSpy.getIsOffline.and.returnValue(of(false));
+        snackbarService = TestBed.inject(SnackbarService);
+        fetchService = TestBed.inject(FetchService) as jasmine.SpyObj<FetchService>;
+        service = TestBed.inject(DataService)
     })
-    service = TestBed.inject(DataService)
-  })
 
-  it('should be created', () => {
-    expect(service).toBeTruthy()
-  })
+    it('should be created', () => {
+        expect(service).toBeTruthy()
+    })
 })
