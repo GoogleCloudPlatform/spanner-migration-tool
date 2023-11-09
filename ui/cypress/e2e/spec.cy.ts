ffa1aaf1a42f02de9c65bcd3769c78f077c2823b
Shreya Khajanchi <khajanchi@google.com>
2023-11-09 11:38:23 +0530

    updated cypress test

--- ui/cypress/e2e/spec.cy.ts
+++ ui/cypress/e2e/spec.cy.ts
@@ -1,11 +1,10 @@
 describe('template spec', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:4200/'); // Adjust the URL to match your Angular app's login page URL
+    cy.visit('http://localhost:4200/');
     cy.fixture('config.json').as('configData');
   });
 
-  // Test Case 1: Check if the login form elements are present
-  it('verify page elements', () => {
+  it('verify direct connection to mysql non-sharded database', () => {
     cy.get('.primary-header').eq(0).should('have.text', 'Get started with Spanner migration tool');
 
     cy.get('#edit-icon').should('exist').click();
@@ -14,38 +13,43 @@ describe('template spec', () => {
       cy.get('#project-id').clear().type(json.projectId)
       cy.get('#instance-id').clear().type(json.instanceId)
     })
-    
+
     cy.get('#save-button').click();
-    cy.wait(5000);
-    cy.get('#check-icon', { timeout: 10000 }).should('exist');
+    cy.get('#save-button', { timeout: 50000 }).should("not.exist");
+    cy.get('#check-icon').should('exist');
     cy.get('#connect-to-database-btn', { timeout: 10000 }).should('exist');
     cy.get('#connect-to-database-btn').click();
 
-    // Wait for the connection to complete (you may need to adjust the wait time)
+    // Wait for the connection to complete
     cy.get('#direct-connection-component', { timeout: 10000 }).should('be.visible');
 
-    // Assuming there are input fields for database type, hostname, username, and password
     cy.get('#dbengine-input').click();
     cy.get('mat-option').contains('MySQL').click();
-    cy.get('#hostname-input').type('localhost');
-    cy.get('#username-input').type('root');
-    cy.get('#password-input').type('pass123');
-    cy.get('#port-input').type('3307');
-    cy.get('#dbname-input').type('test_interleave_table_data');
+
+    cy.fixture('mysql-config').then((json) => {
+      cy.get('#hostname-input').clear().type(json.hostname)
+      cy.get('#username-input').clear().type(json.username)
+      cy.get('#password-input').clear().type(json.password)
+      cy.get('#port-input').clear().type(json.port)
+      cy.get('#dbname-input').clear().type(json.dbName)
+    })
+
     cy.get('#spanner-dialect-input').click();
-    cy.get('mat-option').contains('Google Standard SQL Dialect').click();
+    cy.fixture('constants').then((json) => {
+      cy.get('mat-option').contains(json.googleDialect).click();
+    })
 
     // Check if the button is enabled
-    cy.get('#test-connect-btn').should('be.enabled').then((button) => {
-      // Output the result (true if enabled, false if disabled)
-      const isButtonEnabled = Cypress.$(button).is(':enabled');
-      console.log('Is button enabled:', isButtonEnabled);
-    });
+    cy.get('#test-connect-btn').should('be.enabled')
 
     // Submit the form
     cy.get('#test-connect-btn').click();
     cy.get('#connect-btn').click();
 
-    // No need to quit Cypress; it handles browser instances automatically
+    // Check that workspace is rendered with 2 tables in Object Viewer
+    cy.url().should('include', '/workspace');
+    cy.fixture('mysql-config').then((json) => {
+      cy.get('#table-and-index-list').find('tbody tr').should('have.length', json.tableCount + 2);
+    })
   });
 });
