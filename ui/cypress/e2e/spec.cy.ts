00285004f9f4cb788ba33aab3e858b419ab0fb27
Shreya Khajanchi <khajanchi@google.com>
2023-12-02 16:10:38 +0530

    addressed comments

--- ui/cypress/e2e/spec.cy.ts
+++ ui/cypress/e2e/spec.cy.ts
@@ -1,15 +1,35 @@
+import mockIConv from "../../src/mocks/conv";
+
 describe('template spec', () => {
+  let url = window.location.origin;
   beforeEach(() => {
-    cy.visit('http://localhost:8080/');
-    cy.fixture('config.json').as('configData');
+    // Intercept the backend APIs and return desired response.
+    cy.intercept('GET', `${url}/IsOffline`, { statusCode: 200, body: false }).as('getIsOffline');
+    cy.intercept('GET', `${url}/GetSessions`, { statusCode: 200, body: [] }).as('getSessions');
+    cy.intercept('GET', `${url}/GetConfig`, { statusCode: 200 }).as('getConfig');
+    cy.intercept('GET', `${url}/GetLatestSessionDetails`, { statusCode: 200 }).as('getLatestSessionDetails');
+    cy.visit('http://localhost:4200/');
   });
 
   it('verify direct connection to mysql non-sharded database', () => {
-    cy.get('.primary-header').eq(0).should('have.text', 'Get started with Spanner migration tool');
+    cy.intercept('GET', `${url}/ping`, { statusCode: 200 }).as('checkBackendHealth');
+    cy.intercept('GET', `${url}/convert/infoschema`, { statusCode: 200, body: mockIConv }).as('directConnection');
 
+    cy.get('.primary-header').eq(0).should('have.text', 'Get started with Spanner migration tool');
     cy.get('#edit-icon').should('exist').click();
 
     cy.fixture('config').then((json) => {
+      cy.intercept('POST', `${url}/SetSpannerConfig`, (req) => {
+        req.reply({
+          status: 200,
+          body: {
+            GCPProjectID: json.projectId,
+            SpannerInstanceID: json.instanceId,
+            IsMetadataDbCreated: false,
+            IsConfigValid: true,
+          },
+        });
+      }).as('setSpannerConfig');
       cy.get('#project-id').clear().type(json.projectId)
       cy.get('#instance-id').clear().type(json.instanceId)
     })
@@ -27,6 +47,18 @@ describe('template spec', () => {
     cy.get('mat-option').contains('MySQL').click();
 
     cy.fixture('mysql-config').then((json) => {
+      cy.intercept('POST', `${url}/connect`, (req) => {
+        if (req.body && req.body.Host === json.hostname && req.body.User === json.username && req.body.Driver === 'mysql'
+          && req.body.Password === json.password && req.body.Port === json.port && req.body.Database === json.dbName) {
+          req.reply({
+            statusCode: 200,
+          });
+        } else {
+          req.reply({
+            statusCode: 400,
+          });
+        }
+      }).as('testConnection');
       cy.get('#hostname-input').clear().type(json.hostname)
       cy.get('#username-input').clear().type(json.username)
       cy.get('#password-input').clear().type(json.password)
@@ -44,9 +76,10 @@ describe('template spec', () => {
 
     // Submit the form
     cy.get('#test-connect-btn').click();
+    cy.get('#connect-btn', { timeout: 10000 }).should('be.enabled')
     cy.get('#connect-btn').click();
 
-    // Check that workspace is rendered with 2 tables in Object Viewer
+    // Check that workspace is rendered with correct number of tables in Object Viewer
     cy.url().should('include', '/workspace');
     cy.fixture('mysql-config').then((json) => {
       cy.get('#table-and-index-list').find('tbody tr').should('have.length', json.tableCount + 2);
