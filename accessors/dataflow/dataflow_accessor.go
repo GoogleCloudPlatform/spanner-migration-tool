86daf581075742e41cd750e36ec54e057f00be44
Deep1998 <deepchowdhury1998@gmail.com>
2024-01-23 16:15:50 +0530

    Make dataflow accessor interface and struct to make it testable

--- accessors/dataflow/dataflow_accessor.go
+++ accessors/dataflow/dataflow_accessor.go
@@ -20,16 +20,27 @@ import (
 	"cloud.google.com/go/dataflow/apiv1beta3/dataflowpb"
 	dataflowclient "github.com/GoogleCloudPlatform/spanner-migration-tool/accessors/clients/dataflow"
 	"github.com/GoogleCloudPlatform/spanner-migration-tool/logger"
+	"github.com/googleapis/gax-go/v2"
 )
 
-func LaunchDataflowJob(ctx context.Context, launchRequest *dataflowpb.LaunchFlexTemplateRequest) (*dataflowpb.LaunchFlexTemplateResponse, error) {
+type DataflowAccessor interface {
+	LaunchFlexTemplate(ctx context.Context, req *dataflowpb.LaunchFlexTemplateRequest, opts ...gax.CallOption) (*dataflowpb.LaunchFlexTemplateResponse, error)
+}
+
+func NewDataflowAccessor() DataflowAccessor {
+	return DataflowAccessorImpl{}
+}
+
+type DataflowAccessorImpl struct{}
+
+func (dfA DataflowAccessorImpl) LaunchFlexTemplate(ctx context.Context, req *dataflowpb.LaunchFlexTemplateRequest, opts ...gax.CallOption) (*dataflowpb.LaunchFlexTemplateResponse, error) {
 	dfClient, err := dataflowclient.GetOrCreateClient(ctx)
 	if err != nil {
 		return nil, err
 	}
-	respDf, err := dfClient.LaunchFlexTemplate(ctx, launchRequest)
+	respDf, err := dfClient.LaunchFlexTemplate(ctx, req)
 	if err != nil {
-		logger.Log.Error(fmt.Sprintf("flexTemplateRequest: %+v\n", launchRequest))
+		logger.Log.Error(fmt.Sprintf("flexTemplateRequest: %+v\n", req))
 		return nil, fmt.Errorf("error launching dataflow template: %v", err)
 	}
 	return respDf, nil
