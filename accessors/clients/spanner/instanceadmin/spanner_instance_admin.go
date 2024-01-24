8dff86315cf24bfc902b3fcdb5899f134bd93701
Deep1998 <deepchowdhury1998@gmail.com>
2024-01-24 14:13:18 +0530

    Add spanner instance admin client unit tests

--- accessors/clients/spanner/instanceadmin/spanner_instance_admin.go
+++ accessors/clients/spanner/instanceadmin/spanner_instance_admin.go
@@ -24,11 +24,15 @@ import (
 var once sync.Once
 var instanceAdminClient *instance.InstanceAdminClient
 
+// This function is declared as a global variable to make it testable. The unit
+// tests edit this function, acting like a double.
+var newInstanceAdminClient = instance.NewInstanceAdminClient
+
 func GetOrCreateClient(ctx context.Context) (*instance.InstanceAdminClient, error) {
 	var err error
 	if instanceAdminClient == nil {
 		once.Do(func() {
-			instanceAdminClient, err = instance.NewInstanceAdminClient(ctx)
+			instanceAdminClient, err = newInstanceAdminClient(ctx)
 		})
 		if err != nil {
 			return nil, fmt.Errorf("failed to create spanner instance admin client: %v", err)
--- accessors/clients/spanner/instanceadmin/spanner_instance_admin_test.go
+++ accessors/clients/spanner/instanceadmin/spanner_instance_admin_test.go
@@ -12,3 +12,105 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package spinstanceadmin
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sync"
+	"testing"
+
+	instance "cloud.google.com/go/spanner/admin/instance/apiv1"
+	"github.com/GoogleCloudPlatform/spanner-migration-tool/logger"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"google.golang.org/api/option"
+)
+
+func init() {
+	logger.Log = zap.NewNop()
+}
+
+func TestMain(m *testing.M) {
+	res := m.Run()
+	os.Exit(res)
+}
+
+func resetTest() {
+	instanceAdminClient = nil
+	once = sync.Once{}
+}
+
+func TestGetOrCreateClient_Basic(t *testing.T) {
+	resetTest()
+	ctx := context.Background()
+	oldFunc := newInstanceAdminClient
+	defer func() { newInstanceAdminClient = oldFunc }()
+	newInstanceAdminClient = func(ctx context.Context, opts ...option.ClientOption) (*instance.InstanceAdminClient, error) {
+		return &instance.InstanceAdminClient{}, nil
+	}
+	c, err := GetOrCreateClient(ctx)
+	assert.NotNil(t, c)
+	assert.Nil(t, err)
+}
+
+func TestGetOrCreateClient_OnlyOnceViaSync(t *testing.T) {
+	resetTest()
+	ctx := context.Background()
+	oldFunc := newInstanceAdminClient
+	defer func() { newInstanceAdminClient = oldFunc }()
+
+	newInstanceAdminClient = func(ctx context.Context, opts ...option.ClientOption) (*instance.InstanceAdminClient, error) {
+		return &instance.InstanceAdminClient{}, nil
+	}
+	c, err := GetOrCreateClient(ctx)
+	assert.NotNil(t, c)
+	assert.Nil(t, err)
+	// Explicitly set the client to nil. Running GetOrCreateClient should not create a
+	// new client since sync would already be executed.
+	instanceAdminClient = nil
+	newInstanceAdminClient = func(ctx context.Context, opts ...option.ClientOption) (*instance.InstanceAdminClient, error) {
+		return nil, fmt.Errorf("test error")
+	}
+	c, err = GetOrCreateClient(ctx)
+	assert.Nil(t, c)
+	assert.Nil(t, err)
+}
+
+func TestGetOrCreateClient_OnlyOnceViaIf(t *testing.T) {
+	resetTest()
+	ctx := context.Background()
+	oldFunc := newInstanceAdminClient
+	defer func() { newInstanceAdminClient = oldFunc }()
+
+	newInstanceAdminClient = func(ctx context.Context, opts ...option.ClientOption) (*instance.InstanceAdminClient, error) {
+		return &instance.InstanceAdminClient{}, nil
+	}
+	oldC, err := GetOrCreateClient(ctx)
+	assert.NotNil(t, oldC)
+	assert.Nil(t, err)
+
+	// Explicitly reset once. Running GetOrCreateClient should not create a
+	// new client the if condition should prevent it.
+	once = sync.Once{}
+	newInstanceAdminClient = func(ctx context.Context, opts ...option.ClientOption) (*instance.InstanceAdminClient, error) {
+		return nil, fmt.Errorf("test error")
+	}
+	newC, err := GetOrCreateClient(ctx)
+	assert.Equal(t, oldC, newC)
+	assert.Nil(t, err)
+}
+
+func TestGetOrCreateClient_Error(t *testing.T) {
+	resetTest()
+	ctx := context.Background()
+	oldFunc := newInstanceAdminClient
+	defer func() { newInstanceAdminClient = oldFunc }()
+
+	newInstanceAdminClient = func(ctx context.Context, opts ...option.ClientOption) (*instance.InstanceAdminClient, error) {
+		return nil, fmt.Errorf("test error")
+	}
+	c, err := GetOrCreateClient(ctx)
+	assert.Nil(t, c)
+	assert.NotNil(t, err)
+}
