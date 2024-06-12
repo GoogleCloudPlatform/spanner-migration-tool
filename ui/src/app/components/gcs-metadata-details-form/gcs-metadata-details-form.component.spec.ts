a2844f002aae3100f6e8091500379a20f17f1505
Astha Mohta <asthamohta@google.com>
2024-06-12 22:20:46 +0530

    test

--- ui/src/app/components/gcs-metadata-details-form/gcs-metadata-details-form.component.spec.ts
+++ ui/src/app/components/gcs-metadata-details-form/gcs-metadata-details-form.component.spec.ts
@@ -2,32 +2,31 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-
 import { GcsMetadataDetailsFormComponent } from './gcs-metadata-details-form.component';
+import { TargetDetails, MigrationDetails } from 'src/app/app.constants';
 
 describe('GcsMetadataDetailsFormComponent', () => {
   let component: GcsMetadataDetailsFormComponent;
   let fixture: ComponentFixture<GcsMetadataDetailsFormComponent>;
+  let dialogRefSpy: jasmine.SpyObj<MatDialogRef<GcsMetadataDetailsFormComponent>>;
 
   beforeEach(async () => {
+    dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+
     await TestBed.configureTestingModule({
-      declarations: [ GcsMetadataDetailsFormComponent ],
+      declarations: [GcsMetadataDetailsFormComponent],
       imports: [ReactiveFormsModule, MatSnackBarModule],
       providers: [
         {
           provide: MatDialogRef,
-          useValue: {
-            close: () => {},
-          },
+          useValue: dialogRefSpy,
         },
         {
           provide: MAT_DIALOG_DATA,
-          useValue: {
-          }
-        }
+          useValue: {},
+        },
       ],
-    })
-    .compileComponents();
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -38,5 +37,20 @@ describe('GcsMetadataDetailsFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update localStorage and close the dialog', () => {
+    spyOn(localStorage, 'setItem');
+    component.gcsMetadataDetailsForm.setValue({
+      gcsName: 'testGcsName',
+      gcsRootPath: 'testGcsRootPath'
+    });
+
+    component.updateGcsPathMetadataDetails();
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(TargetDetails.GcsMetadataName, 'testGcsName');
+    expect(localStorage.setItem).toHaveBeenCalledWith(TargetDetails.GcsMetadataRootPath, 'testGcsRootPath');
+    expect(localStorage.setItem).toHaveBeenCalledWith(MigrationDetails.IsGcsMetadataPathSet, 'true');
+    expect(dialogRefSpy.close).toHaveBeenCalled();
   });
 });
