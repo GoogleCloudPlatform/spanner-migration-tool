c030f011d32f7906a7d12a4f6f3d76a1bc91b187
Manit Gupta <manitgupta@google.com>
2024-11-26 13:30:28 +0530

    Address comments and rebase

--- expressions_api/expression_verify.go
+++ expressions_api/expression_verify.go
@@ -9,9 +9,8 @@ import (
 	spannerclient "github.com/GoogleCloudPlatform/spanner-migration-tool/accessors/clients/spanner/client"
 	spanneraccessor "github.com/GoogleCloudPlatform/spanner-migration-tool/accessors/spanner"
 	"github.com/GoogleCloudPlatform/spanner-migration-tool/common/constants"
+	"github.com/GoogleCloudPlatform/spanner-migration-tool/common/task"
 	"github.com/GoogleCloudPlatform/spanner-migration-tool/internal"
-	"github.com/GoogleCloudPlatform/spanner-migration-tool/sources/common"
-	"github.com/GoogleCloudPlatform/spanner-migration-tool/spanner/ddl"
 )
 
 const THREAD_POOL = 500
@@ -22,7 +21,17 @@ type ExpressionVerificationAccessor interface {
 }
 
 type ExpressionVerificationAccessorImpl struct {
-	SpannerAccessor spanneraccessor.SpannerAccessorImpl
+	SpannerAccessor *spanneraccessor.SpannerAccessorImpl
+}
+
+func NewExpressionVerificationAccessorImpl(ctx context.Context, project string, instance string) (*ExpressionVerificationAccessorImpl, error) {
+	spannerAccessor, err := spanneraccessor.NewSpannerAccessorClientImplWithSpannerClient(ctx, fmt.Sprintf("projects/%s/instances/%s", project, instance))
+	if err != nil {
+		return nil, err
+	}
+	return &ExpressionVerificationAccessorImpl{
+		SpannerAccessor: spannerAccessor,
+	}, nil
 }
 
 // This is an internal struct to the API implementation and should not leak out of the spanneraccessor package (member fields are not exported)
@@ -36,7 +45,7 @@ func (ev *ExpressionVerificationAccessorImpl) VerifyExpressions(ctx context.Cont
 	if err != nil {
 		return internal.VerifyExpressionsOutput{Err: err}
 	}
-	dbURI := fmt.Sprintf("projects/%s/instances/%s/databases/%s", verifyExpressionsInput.Project, verifyExpressionsInput.Instance, ev.SpannerAccessor.SpannerClient.DatabaseName())
+	dbURI := ev.SpannerAccessor.SpannerClient.DatabaseName()
 	dbExists, err := ev.SpannerAccessor.CheckExistingDb(ctx, dbURI)
 	if err != nil {
 		return internal.VerifyExpressionsOutput{Err: err}
@@ -47,7 +56,7 @@ func (ev *ExpressionVerificationAccessorImpl) VerifyExpressions(ctx context.Cont
 			return internal.VerifyExpressionsOutput{Err: err}
 		}
 	}
-	verifyExpressionsInput.Conv, err = ev.simplifyConv(verifyExpressionsInput.Conv)
+	verifyExpressionsInput.Conv, err = ev.removeExpressions(verifyExpressionsInput.Conv)
 	if err != nil {
 		return internal.VerifyExpressionsOutput{Err: err}
 	}
@@ -64,7 +73,7 @@ func (ev *ExpressionVerificationAccessorImpl) VerifyExpressions(ctx context.Cont
 			expressionDetail: expressionDetail,
 		}
 	}
-	r := common.RunParallelTasksImpl[ExpressionVerificationInput, internal.ExpressionVerificationOutput]{}
+	r := task.RunParallelTasksImpl[ExpressionVerificationInput, internal.ExpressionVerificationOutput]{}
 	expressionVerificationOutputList, _ := r.RunParallelTasks(verificationInputList, THREAD_POOL, ev.verifyExpressionInternal, true)
 	var verifyExpressionsOutput internal.VerifyExpressionsOutput
 	var errorCount int16 = 0
@@ -81,23 +90,23 @@ func (ev *ExpressionVerificationAccessorImpl) VerifyExpressions(ctx context.Cont
 	return verifyExpressionsOutput
 }
 
-func (ev *ExpressionVerificationAccessorImpl) verifyExpressionInternal(expressionVerificationInput ExpressionVerificationInput, mutex *sync.Mutex) common.TaskResult[internal.ExpressionVerificationOutput] {
+func (ev *ExpressionVerificationAccessorImpl) verifyExpressionInternal(expressionVerificationInput ExpressionVerificationInput, mutex *sync.Mutex) task.TaskResult[internal.ExpressionVerificationOutput] {
 	var sqlStatement string
 	switch expressionVerificationInput.expressionDetail.Type {
 	case "CHECK":
 		sqlStatement = fmt.Sprintf("SELECT 1 from %s where %s;", expressionVerificationInput.expressionDetail.ReferenceElement.Name, expressionVerificationInput.expressionDetail.Expression)
 	case "DEFAULT":
-		sqlStatement = fmt.Sprintf("SELECT CAST(%s) as %s", expressionVerificationInput.expressionDetail.Expression, expressionVerificationInput.expressionDetail.ReferenceElement.Name)
+		sqlStatement = fmt.Sprintf("SELECT CAST(%s as %s)", expressionVerificationInput.expressionDetail.Expression, expressionVerificationInput.expressionDetail.ReferenceElement.Name)
 	default:
-		return common.TaskResult[internal.ExpressionVerificationOutput]{Result: internal.ExpressionVerificationOutput{Result: false, Err: fmt.Errorf("invalid expression type requested")}, Err: nil}
+		return task.TaskResult[internal.ExpressionVerificationOutput]{Result: internal.ExpressionVerificationOutput{Result: false, Err: fmt.Errorf("invalid expression type requested")}, Err: nil}
 	}
 	result, err := ev.SpannerAccessor.ValidateDML(context.Background(), sqlStatement)
-	return common.TaskResult[internal.ExpressionVerificationOutput]{Result: internal.ExpressionVerificationOutput{Result: result, Err: err, ExpressionDetail: expressionVerificationInput.expressionDetail}, Err: nil}
+	return task.TaskResult[internal.ExpressionVerificationOutput]{Result: internal.ExpressionVerificationOutput{Result: result, Err: err, ExpressionDetail: expressionVerificationInput.expressionDetail}, Err: nil}
 }
 
 func (ev *ExpressionVerificationAccessorImpl) validateRequest(verifyExpressionsInput internal.VerifyExpressionsInput) error {
-	if verifyExpressionsInput.Project == "" || verifyExpressionsInput.Instance == "" || verifyExpressionsInput.Conv == nil || verifyExpressionsInput.Source == "" {
-		return fmt.Errorf("one of project, instance, conv or source is empty. These are mandatory fields = %v", verifyExpressionsInput)
+	if verifyExpressionsInput.Conv == nil || verifyExpressionsInput.Source == "" {
+		return fmt.Errorf("one of conv or source is empty. These are mandatory fields = %v", verifyExpressionsInput)
 	}
 	for _, expressionDetail := range verifyExpressionsInput.ExpressionDetailList {
 		if expressionDetail.ExpressionId == "" || expressionDetail.Expression == "" || expressionDetail.Type == "" || expressionDetail.ReferenceElement.Name == "" {
@@ -110,7 +119,7 @@ func (ev *ExpressionVerificationAccessorImpl) validateRequest(verifyExpressionsI
 // We simplify conv to remove any existing expressions that are part of the SpSchema to ensure that the stagingDB creation
 // does not fail due to inconsistent, user configured expressions during a schema conversion session.
 // The minimal conv object needed for stagingDB is one which contains all table and column definitions only.
-func (ev *ExpressionVerificationAccessorImpl) simplifyConv(inputConv *internal.Conv) (*internal.Conv, error) {
+func (ev *ExpressionVerificationAccessorImpl) removeExpressions(inputConv *internal.Conv) (*internal.Conv, error) {
 	convCopy := &internal.Conv{}
 	convJSON, err := json.Marshal(inputConv)
 	if err != nil {
@@ -120,11 +129,8 @@ func (ev *ExpressionVerificationAccessorImpl) simplifyConv(inputConv *internal.C
 	if err != nil {
 		return nil, fmt.Errorf("error unmarshaling conv: %v", err)
 	}
-	// Reset the SpSequences field in the Conv copy.  A nil check
-	// here won't hurt but shouldn't technically be necessary as the
-	// map should always be initialized.
-	if convCopy.SpSequences != nil {
-		convCopy.SpSequences = make(map[string]ddl.Sequence) // Reset to empty map
-	}
+	// TODO: Add logic here which removes expressions from the convCopy object and
+	// returns it back. This will include logic for DEFAULT and CHECK constraints,
+	// and sequences.
 	return convCopy, nil
 }
