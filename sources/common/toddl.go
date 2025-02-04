e63b5845f390ce8d68768ce2425b27a9908381e0
Astha Mohta <35952883+asthamohta@users.noreply.github.com>
2025-02-04 17:23:23 +0530

    fix: Spanner Client refresh fix (#1000)
    * Changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes

--- sources/common/toddl.go
+++ sources/common/toddl.go
@@ -91,14 +91,6 @@ func (ss *SchemaToSpannerImpl) SchemaToSpannerDDL(conv *internal.Conv, toddl ToD
 		conv.AddShardIdColumn()
 	}
 
-	if (conv.Source == constants.MYSQL || conv.Source == constants.MYSQLDUMP) && conv.SpProjectId != "" && conv.SpInstanceId != "" {
-		// Process and verify Check constraints for MySQL and MySQLDump flow only
-		err := ss.VerifyExpressions(conv)
-		if err != nil {
-			return err
-		}
-	}
-
 	if conv.Source == constants.MYSQL && conv.SpProjectId != "" && conv.SpInstanceId != "" {
 		// Process and verify Spanner DDL expressions for MYSQL
 		expressionDetails := ss.DdlV.GetSourceExpressionDetails(conv, tableIds)
@@ -108,6 +100,15 @@ func (ss *SchemaToSpannerImpl) SchemaToSpannerDDL(conv *internal.Conv, toddl ToD
 		}
 		spannerSchemaApplyExpressions(conv, expressions)
 	}
+
+	if (conv.Source == constants.MYSQL || conv.Source == constants.MYSQLDUMP) && conv.SpProjectId != "" && conv.SpInstanceId != "" {
+		// Process and verify Check constraints for MySQL and MySQLDump flow only
+		err := ss.VerifyExpressions(conv)
+		if err != nil {
+			return err
+		}
+	}
+
 	internal.ResolveRefs(conv)
 	return nil
 }
@@ -255,6 +256,7 @@ func (ss *SchemaToSpannerImpl) VerifyExpressions(conv *internal.Conv) error {
 		Source:               conv.Source,
 		ExpressionDetailList: GenerateExpressionDetailList(spschema),
 	}
+	ss.ExpressionVerificationAccessor.RefreshSpannerClient(ctx, conv.SpProjectId, conv.SpInstanceId)
 	if len(verifyExpressionsInput.ExpressionDetailList) != 0 {
 		result := ss.ExpressionVerificationAccessor.VerifyExpressions(ctx, verifyExpressionsInput)
 		if result.ExpressionVerificationOutputList == nil {
