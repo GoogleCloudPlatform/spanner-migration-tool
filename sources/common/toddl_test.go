96d548c814a92340e15b2492b1aeb801168354de
asthamohta <asthamohta@google.com>
2025-02-04 09:24:11 +0000

    changes

--- sources/common/toddl_test.go
+++ sources/common/toddl_test.go
@@ -715,7 +715,7 @@ func TestVerifyCheckConstraintExpressions(t *testing.T) {
 			mockAccessor.On("VerifyExpressions", ctx, mock.Anything).Return(internal.VerifyExpressionsOutput{
 				ExpressionVerificationOutputList: tc.expectedResults,
 			})
-			mockAccessor.On("RefreshSpannerClient", ctx, mock.Anything).Return()
+			mockAccessor.On("RefreshSpannerClient", ctx, mock.Anything, ctx, mock.Anything, ctx, mock.Anything).Return()
 			handler.VerifyExpressions(conv)
 			assert.Equal(t, conv.SpSchema["t1"].CheckConstraints, tc.expectedCheckConstraint)
 
