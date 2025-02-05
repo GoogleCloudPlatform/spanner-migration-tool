263e94225114553f933439978963477013b36ede
Taher Lakdawala <78196491+taherkl@users.noreply.github.com>
2025-02-05 15:20:49 +0530

    Fix: For session flag added the error handling (#88) (#1005)
    * Fix: for the session flag added the error handling
    
    * updated the log message
    
    ---------
    
    Co-authored-by: Vivek Yadav <105432992+VivekY1098@users.noreply.github.com>
    Co-authored-by: Vivek Yadav <vivek.yadav@ollion.com>

--- cmd/schema.go
+++ cmd/schema.go
@@ -142,6 +142,7 @@ func (cmd *SchemaCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interfa
 		err := schemaToSpanner.VerifyExpressions(conv)
 
 		if err != nil {
+			logger.Log.Error(fmt.Sprintf("Error while verifying the expressions %v", err))
 			return subcommands.ExitFailure
 		}
 	} else {
