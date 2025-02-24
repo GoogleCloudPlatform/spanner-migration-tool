4d0900756b2fe11b3bed146c98f9d5587ac9a8b5
Svetak Sundhar <svetaksundhar@google.com>
2025-02-24 12:40:48 -0500

    refactor

--- cmd/schema_and_data_test.go
+++ cmd/schema_and_data_test.go
@@ -1,3 +1,17 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cmd
 
 import (
@@ -27,34 +41,6 @@ func TestSchemaAndDataSetFlags(t *testing.T) {
                                 logLevel:         "DEBUG",
                                 SkipForeignKeys:  false,
                                 validate:         false,
-                                dataflowTemplate: constants.DEFAULT_TEMPLATE_PATH,
-                        },
-                },
-                {
-                        testName: "Non-Default Values",
-                        flagArgs:string{
-                            "-source=MySQL",
-                            "-source-profile=file=test.sql",
-                            "-target=Spanner",
-                            "-target-profile=instance=test-instance",
-                            "-prefix=test-prefix",
-                            "-write-limit=1000",
-                            "-dry-run",
-                            "-log-level=INFO",
-                            "-skip-foreign-keys",
-                            "-validate",
-                        },
-                        expectedValues: SchemaAndDataCmd{
-                                source:           "MySQL",
-                                sourceProfile:    "file=test.sql",
-                                target:           "Spanner",
-                                targetProfile:    "instance=test-instance",
-                                filePrefix:       "test-prefix",
-                                WriteLimit:       1000,
-                                dryRun:           true,
-                                logLevel:         "INFO",
-                                SkipForeignKeys:  true,
-                                validate:         true,
                                 dataflowTemplate: constants.DEFAULT_TEMPLATE_PATH,
                         },
                 },
