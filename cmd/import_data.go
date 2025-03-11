9ba684a1fc2d216115896bb3071cd21f8dd67174
Rohit Wali <rohitwali@google.com>
2025-03-11 16:35:16 +0000

    Adding initial interface for import data with sample csv file and schema

--- cmd/import_data.go
+++ cmd/import_data.go
@@ -1,3 +1,17 @@
+/* Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.*/
+
 package cmd
 
 import (
@@ -9,38 +23,49 @@ import (
 )
 
 type ImportDataCmd struct {
-	source            string
-	sourceProfile     string
-	target            string
-	targetProfile     string
-	assessmentProfile string
+	instanceId        string
+	dbName            string
+	tableName         string
+	sourceUri         string
+	sourceFormat      string
+	schemaUri         string
+	csvLineDelimiter  string
+	csvFieldDelimiter string
 	project           string
-	logLevel          string
-	dryRun            bool
-	validate          bool
-	sessionJSON       string
 }
 
+// https://source.corp.google.com/piper///depot/google3/third_party/py/googlecloudsdk/surface/
+
 func (cmd *ImportDataCmd) SetFlags(set *flag.FlagSet) {
-	//TODO implement me
-	panic("implement me")
+	set.StringVar(&cmd.instanceId, "instance-id", "", "Spanner instance Id")
+	set.StringVar(&cmd.dbName, "db-name", "", "Spanner database name")
+	set.StringVar(&cmd.tableName, "table-name", "", "Spanner table name")
+	set.StringVar(&cmd.sourceUri, "source-uri", "", "URI of the file to import")
+	set.StringVar(&cmd.sourceFormat, "format", "", "Format of the file to import. Valid values {csv}")
+	set.StringVar(&cmd.schemaUri, "schema-uri", "", "URI of the file with schema for the csv to import. Only used for csv format.")
+	set.StringVar(&cmd.csvLineDelimiter, "csv-line-delimiter", "", "Token to be used as line delimiter for csv format. Defaults to '\\n'. Only used for csv format.")
+	set.StringVar(&cmd.csvFieldDelimiter, "csv-field-delimiter", "", "Token to be used as field delimiter for csv format. Defaults to ','. Only used for csv format.")
+	set.StringVar(&cmd.project, "project", "", "Project id for all resources related to this import")
 }
 
 func (cmd *ImportDataCmd) Execute(ctx context.Context, f *flag.FlagSet, args ...interface{}) subcommands.ExitStatus {
 	//TODO implement me
-	panic("implement me")
+	fmt.Printf("executing dummy import data\n")
+	fmt.Printf("instanceId %s, dbName %s, schemaUri %s\n", cmd.instanceId, cmd.dbName, cmd.schemaUri)
+	return 0
 }
 
 func (cmd *ImportDataCmd) Name() string {
-	return "import"
+	return "Import Data"
 }
 
 // Synopsis returns summary of operation.
 func (cmd *ImportDataCmd) Synopsis() string {
-	return "import data to spanner"
+	return "Import data from supported source files to spanner"
 }
 
 // Usage returns usage info of the command.
 func (cmd *ImportDataCmd) Usage() string {
+	//TODO implement me
 	return fmt.Sprintf("test usage")
 }
--- testing/import_data/integration_test.go
+++ testing/import_data/integration_test.go
@@ -1,3 +1,17 @@
+/* Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.*/
+
 package import_data_test
 
 import (
@@ -65,6 +79,33 @@ func TestLocalCSVFile(t *testing.T) {
 	// write new csv data to spanner
 	// just trigger the csv command
 	MANIFEST_FILE_NAME := "../../test_data/csv_test2.json"
+	// instance id - ok
+	// database name - ok
+	// table name
+	// source uri
+	// format - ok
+	// schema uri
+	// csv line delimiter
+	// csv field delimiter
+
+	/*
+		MANIFESTFILE
+		[
+		  {
+		    "table_name": "table2",
+		    "file_patterns": [
+		      "/usr/local/google/home/rohitwali/dump1/tabletest.txt"
+		    ],
+		    "columns": [
+		      {"column_name": "c3", "type_name": "INT64"},
+		      {"column_name": "c4", "type_name": "STRING"}
+		    ]
+		  }
+		]
+
+	*/
+
+	// project id  -  need to find out from gcloud and pass
 	args := fmt.Sprintf("data -source=csv -source-profile=manifest=%s -target-profile='instance=%s,dbName=%s,project=%s'", MANIFEST_FILE_NAME, instanceID, dbName, projectID)
 	err = common.RunCommand(args, projectID)
 	if err != nil {
