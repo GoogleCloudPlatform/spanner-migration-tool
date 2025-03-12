0f2e7b7d17860e9f4a196f63ebff5f22b8ef9a7f
Rohit Wali <rohitwali@google.com>
2025-03-12 14:17:44 +0000

    one more - fixing debug log formatting

--- testing/import_data/integration_test.go
+++ testing/import_data/integration_test.go
@@ -88,7 +88,7 @@ func TestLocalCSVFile(t *testing.T) {
 	log.Printf("projectID %s, instanceID %s", projectID, instanceID)
 	projectID := os.Getenv("SPANNER_MIGRATION_TOOL_TESTS_GCLOUD_PROJECT_ID")
 	instanceID := os.Getenv("SPANNER_MIGRATION_TOOL_TESTS_GCLOUD_INSTANCE_ID")
-	log.Println("projectID %s, instanceID %s", projectID, instanceID)
+	log.Printf("projectID %s, instanceID %s", projectID, instanceID)
 	// configure the database client
 	dbName := "versionone"
 	tableName := "table2"
