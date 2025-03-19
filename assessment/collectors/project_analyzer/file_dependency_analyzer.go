8aefd1a51712ea9130dc5a27c05d5f4664b01905
Pratick Chokhani <pratick@google.com>
2025-03-19 21:58:57 +0530

    feat(spanner): Updated Gemini prompts and Parallel processing of files (#1061)
    * feat(spanner): Updated Gemini prompts and Parallel processing of files
    - Gemini prompts are updated to have pagination (only first page is processed as of now) and Json retries
    - Added parallel processing for files that does not depend on each other.

--- assessment/collectors/project_analyzer/file_dependency_analyzer.go
+++ assessment/collectors/project_analyzer/file_dependency_analyzer.go
@@ -51,6 +51,11 @@ func validateGoroot() error {
 	return nil
 }
 
+// packagesLoadLogger: debug logger for packages.Load function
+func packagesLoadLogger(format string, args ...interface{}) {
+	logger.Log.Debug(fmt.Sprintf(format, args...))
+}
+
 func (g *GoDependencyAnalyzer) getDependencyGraph(directory string) map[string]map[string]struct{} {
 
 	err := validateGoroot()
@@ -60,6 +65,7 @@ func (g *GoDependencyAnalyzer) getDependencyGraph(directory string) map[string]m
 	cfg := &packages.Config{
 		Mode: packages.NeedName | packages.NeedFiles | packages.NeedSyntax | packages.NeedTypes | packages.NeedTypesInfo,
 		Dir:  (directory),
+		Logf: packagesLoadLogger,
 	}
 
 	logger.Log.Debug(fmt.Sprintf("loading packages from directory: %s", directory))
