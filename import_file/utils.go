ae38667ca4b1ed50e475030f9c7ea69e517d521b
Pratick Chokhani <pratick@google.com>
2025-04-28 13:02:09 +0530

    chore(spanner): Add support for reading file from GCS.

--- import_file/utils.go
+++ import_file/utils.go
@@ -1,17 +1,50 @@
 package import_file
 
 import (
+	"cloud.google.com/go/storage"
+	"context"
 	"fmt"
 	"github.com/GoogleCloudPlatform/spanner-migration-tool/logger"
 	"os"
 )
 
-func ResetReader(dumpReader *os.File, fileUri string) (*os.File, error) {
-	_, err := dumpReader.Seek(0, 0)
+func ResetReaderFile(f *os.File, fileUri string) (*os.File, error) {
+	_, err := f.Seek(0, 0)
 	if err != nil {
 		logger.Log.Error(fmt.Sprintf("can't reset reader: %v\n", err))
-		dumpReader.Close()
-		dumpReader, err = os.Open(fileUri)
+		f.Close()
+		return os.Open(fileUri)
 	}
-	return dumpReader, err
+	return f, err
+
+}
+
+func CreateDumpFile(fileUri string) (*os.File, error) {
+	f, err := os.Open(fileUri)
+	if err != nil {
+		logger.Log.Error(fmt.Sprintf("readFile: unable to open file: %s. Error: %q", fileUri, err))
+		return nil, err
+	}
+	return f, nil
+}
+
+func ResetGCSReader(ctx context.Context, client *storage.Client, bucketName, filePath string) (*storage.Reader, error) {
+	return client.Bucket(bucketName).Object(filePath[1:]).NewReader(ctx)
+}
+
+func CreateGCSReader(ctx context.Context, bucketName, filePath string) (*storage.Reader, *storage.Client, error) {
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		logger.Log.Error(fmt.Sprintf("Failed to create GCS client for bucket %q", err))
+		return nil, nil, err
+	}
+
+	rc, err := client.Bucket(bucketName).Object(filePath[1:]).NewReader(ctx)
+	if err != nil {
+		defer client.Close()
+		logger.Log.Error(fmt.Sprintf("readFile: unable to open file from bucket %q, file %q: %v", bucketName, filePath, err))
+		return nil, nil, err
+	}
+	return rc, client, nil
 }
