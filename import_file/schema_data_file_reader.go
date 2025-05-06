3cbe883dc4803c766dbc319b2d91f37a44a2475b
Pratick Chokhani <pratick@google.com>
2025-05-06 19:39:01 +0530

    chore(spanner): Removed unused function

--- import_file/schema_data_file_reader.go
+++ import_file/schema_data_file_reader.go
@@ -125,13 +125,3 @@ func (reader *SchemeDataReaderImpl) Close() {
 		reader.file.Close()
 	}
 }
-
-func ResetReader(dumpReader *os.File, fileUri string) (*os.File, error) {
-	_, err := dumpReader.Seek(0, 0)
-	if err != nil {
-		logger.Log.Error(fmt.Sprintf("can't reset reader: %v\n", err))
-		dumpReader.Close()
-		dumpReader, err = os.Open(fileUri)
-	}
-	return dumpReader, err
-}
