885e34a8f377f070c6a890d69c0fbe0a473b75af
djagaluru <djagaluru@google.com>
2025-07-21 10:29:22 +0000

    Fixing binary schema migration for mysql to map to correct spanner type with length BYTES(XX)

--- sources/mysql/mysqldump_test.go
+++ sources/mysql/mysqldump_test.go
@@ -70,7 +70,7 @@ func TestProcessMySQLDump_Scalar(t *testing.T) {
 		{"enum('a','b')", ddl.Type{Name: ddl.String, Len: ddl.MaxLength}},
 		{"timestamp", ddl.Type{Name: ddl.Timestamp}},
 		{"datetime", ddl.Type{Name: ddl.Timestamp}},
-		{"binary", ddl.Type{Name: ddl.Bytes, Len: ddl.MaxLength}},
+		{"binary", ddl.Type{Name: ddl.Bytes, Len: int64(1)}},
 		{"varchar(42)", ddl.Type{Name: ddl.String, Len: int64(42)}},
 	}
 	for _, tc := range scalarTests {
