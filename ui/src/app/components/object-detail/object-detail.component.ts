5f2c12835c81eb40eb01ab4c75b1b94ef9e6da05
shreyakhajanchi <92910380+shreyakhajanchi@users.noreply.github.com>
2025-08-04 11:21:38 +0530

    Fixing primary key sort order for source (#1170)
    * Fixing primary key sort order for source
    
    * addressing comments
    
    * removing console log

--- ui/src/app/components/object-detail/object-detail.component.ts
+++ ui/src/app/components/object-detail/object-detail.component.ts
@@ -252,6 +252,7 @@ export class ObjectDetailComponent implements OnInit {
       this.setColumnsToAdd()
       this.setAddPkColumnList()
       this.setPkOrder()
+      this.setSrcPkOrder()
       this.setPkRows()
       this.setFkRows()
       this.setCCRows()
@@ -808,7 +809,9 @@ export class ObjectDetailComponent implements OnInit {
     spArr.sort((a, b) => {
       return a.spOrder - b.spOrder
     })
-
+    srcArr.sort((a, b) => {
+      return a.srcOrder - b.srcOrder
+    })
     for (let i = 0; i < Math.min(srcArr.length, spArr.length); i++) {
       this.pkArray.push(
         new FormGroup({
@@ -1092,6 +1095,7 @@ export class ObjectDetailComponent implements OnInit {
     })
     this.pkData[index].spOrder = newColumnOrder
     this.setAddPkColumnList()
+    this.setSrcPkOrder()
     this.setPkRows()
   }
 
@@ -1113,23 +1117,9 @@ export class ObjectDetailComponent implements OnInit {
   }
 
   setPkOrder() {
-    if (
-      this.currentObject &&
-      this.conv.SpSchema[this.currentObject!.id]?.PrimaryKeys.length == this.pkData.length
-    ) {
-      this.pkData.forEach((pk: IColumnTabData, i: number) => {
-        if (
-          this.pkData[i].spId === this.conv.SpSchema[this.currentObject!.id].PrimaryKeys[i].ColId
-        ) {
-          this.pkData[i].spOrder = this.conv.SpSchema[this.currentObject!.id].PrimaryKeys[i].Order
-        } else {
-          let index = this.conv.SpSchema[this.currentObject!.id].PrimaryKeys.map(
-            (item) => item.ColId
-          ).indexOf(pk.spId)
-          pk.spOrder = this.conv.SpSchema[this.currentObject!.id].PrimaryKeys[index]?.Order
-        }
-      })
-    } else {
+    if (!this.currentObject || !this.conv.SrcSchema[this.currentObject!.id]?.PrimaryKeys) {
+        return; // Early exit if there's no current object or primary key data
+    }
       this.pkData.forEach((pk: IColumnTabData, i: number) => {
         let index = this.conv.SpSchema[this.currentObject!.id]?.PrimaryKeys.map(
           (item) => item.ColId
@@ -1138,8 +1128,20 @@ export class ObjectDetailComponent implements OnInit {
           pk.spOrder = this.conv.SpSchema[this.currentObject!.id]?.PrimaryKeys[index].Order
         }
       })
-    }
   }
+
+  setSrcPkOrder() {
+    if (!this.currentObject || !this.conv.SrcSchema[this.currentObject!.id]?.PrimaryKeys) {
+        return; // Early exit if there's no current object or primary key data
+    }
+    const srcPrimaryKeys = this.conv.SrcSchema[this.currentObject!.id].PrimaryKeys;
+    this.pkData.forEach((pk: IColumnTabData) => {
+        let index = srcPrimaryKeys.map(item => item.ColId).indexOf(pk.srcId);
+        if (index !== -1) {
+            pk.srcOrder = srcPrimaryKeys[index].Order;
+        }
+    });
+}
 
   pkOrderValidation() {
     let arr = this.pkData
@@ -1204,6 +1206,7 @@ export class ObjectDetailComponent implements OnInit {
       this.pkData = this.conversion.getPkMapping(this.tableData)
       this.setAddPkColumnList()
       this.setPkOrder()
+      this.setSrcPkOrder()
       this.setPkRows()
       this.isPkEditMode = false
     } else {
