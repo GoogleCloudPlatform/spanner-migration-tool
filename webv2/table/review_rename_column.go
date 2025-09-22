ed9562ec0d76bfafb3a233101ae0baf179c9fe2f
Sandeep Mishra <iamsandeep@google.com>
2025-09-22 06:01:53 +0000

    Explicit logging on why the save failed. Don’t allow Renaming / Deleting primary key which would affect existing interleaving Don’t allow Deletion of interleaved table

--- webv2/table/review_rename_column.go
+++ webv2/table/review_rename_column.go
@@ -16,67 +16,11 @@ package table
 
 import (
 	"github.com/GoogleCloudPlatform/spanner-migration-tool/internal"
-	utilities "github.com/GoogleCloudPlatform/spanner-migration-tool/webv2/utilities"
 )
 
 // reviewRenameColumn review  renaming of Columnname in schmema.
-func reviewRenameColumn(newName, tableId, colId string, conv *internal.Conv, interleaveTableSchema []InterleaveTableSchema) []InterleaveTableSchema {
-
-	sp := conv.SpSchema[tableId]
-
-	// review column name update for interleaved child.
-	interleaveTableSchema, childTableId := reviewRenameColumnForChildTable(newName, tableId, colId, conv, interleaveTableSchema)
-
-	// review column name update for interleaved parent.
-	interleaveTableSchema, parentTableId := reviewRenameColumnForParentTable(newName, tableId, colId, conv, interleaveTableSchema)
-
-	oldColName := conv.SpSchema[tableId].ColDefs[colId].Name
-	colType := conv.SpSchema[tableId].ColDefs[colId].T.Name
-	colSize := conv.SpSchema[tableId].ColDefs[colId].T.Len
+func reviewRenameColumn(newName, tableId, colId string, conv *internal.Conv) {
 	reviewRenameColumnNameTableSchema(conv, tableId, colId, newName)
-	if childTableId != "" || parentTableId != "" {
-		interleaveTableSchema = renameInterleaveTableSchema(interleaveTableSchema, sp.Name, colId, oldColName, newName, colType, int(colSize))
-	}
-
-	return interleaveTableSchema
-}
-
-func reviewRenameColumnForChildTable(newName, tableId, colId string, conv *internal.Conv, interleaveTableSchema []InterleaveTableSchema) ([]InterleaveTableSchema, string) {
-	sp := conv.SpSchema[tableId]
-	isParent, childTableId := utilities.IsParent(tableId)
-
-	if isParent {
-		childColId, err := utilities.GetColIdFromSpannerName(conv, childTableId, sp.ColDefs[colId].Name)
-		if err == nil {
-			interleaveTableSchema, _ = reviewRenameColumnForChildTable(newName, childTableId, childColId, conv, interleaveTableSchema)
-			oldColName := conv.SpSchema[childTableId].ColDefs[childColId].Name
-			reviewRenameColumnNameTableSchema(conv, childTableId, childColId, newName)
-			childTableName := conv.SpSchema[childTableId].Name
-			colType := conv.SpSchema[childTableId].ColDefs[childColId].T.Name
-			colSize := conv.SpSchema[childTableId].ColDefs[childColId].T.Len
-			interleaveTableSchema = renameInterleaveTableSchema(interleaveTableSchema, childTableName, childColId, oldColName, newName, colType, int(colSize))
-		}
-	}
-	return interleaveTableSchema, childTableId
-}
-
-func reviewRenameColumnForParentTable(newName, tableId, colId string, conv *internal.Conv, interleaveTableSchema []InterleaveTableSchema) ([]InterleaveTableSchema, string) {
-	sp := conv.SpSchema[tableId]
-	parentTableId := conv.SpSchema[tableId].ParentTable.Id
-
-	if parentTableId != "" {
-		parentColId, err := utilities.GetColIdFromSpannerName(conv, parentTableId, sp.ColDefs[colId].Name)
-		if err == nil {
-			interleaveTableSchema, _ = reviewRenameColumnForParentTable(newName, parentTableId, parentColId, conv, interleaveTableSchema)
-			oldColName := conv.SpSchema[parentTableId].ColDefs[parentColId].Name
-			reviewRenameColumnNameTableSchema(conv, parentTableId, parentColId, newName)
-			parentTableName := conv.SpSchema[parentTableId].Name
-			colType := conv.SpSchema[parentTableId].ColDefs[parentColId].T.Name
-			colSize := conv.SpSchema[parentTableId].ColDefs[parentColId].T.Len
-			interleaveTableSchema = renameInterleaveTableSchema(interleaveTableSchema, parentTableName, parentColId, oldColName, newName, colType, int(colSize))
-		}
-	}
-	return interleaveTableSchema, parentTableId
 }
 
 // reviewRenameColumnNameTableSchema review  renaming of column-name in Table Schema.
